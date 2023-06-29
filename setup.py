15a3e6c37784b68fad39485b9ad9474ee5b28c05
AlpinDale <alpindale@gmail.com>
2023-06-29 21:34:30 +0000

    fix: setuptools fix

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CUDA_HOME
 
 ROOT_DIR = os.path.dirname(__file__)
 
+# Compiler flags.
 CXX_FLAGS = ["-g", "-O2", "-std=c++17"]
-NVCC_FLAGS = ["-O3", "-std=c++17"]
+# TODO(woosuk): Should we use -O3?
+NVCC_FLAGS = ["-O2", "-std=c++17"]
 
 ABI = 1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0
 CXX_FLAGS += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
@@ -22,7 +24,12 @@ if CUDA_HOME is None:
     raise RuntimeError(
         f"Cannot find CUDA_HOME. CUDA must be available in order to build the package.")
 
+
 def get_nvcc_cuda_version(cuda_dir: str) -> Version:
+    """Get the CUDA version from nvcc.
+
+    Adapted from https://github.com/NVIDIA/apex/blob/8b7a1ff183741dd8f9b87e7bafd04cfde99cea28/setup.py
+    """
     nvcc_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"],
                                           universal_newlines=True)
     output = nvcc_output.split()
@@ -30,6 +37,8 @@ def get_nvcc_cuda_version(cuda_dir: str) -> Version:
     nvcc_cuda_version = parse(output[release_idx].split(",")[0])
     return nvcc_cuda_version
 
+
+# Collect the compute capabilities of all available GPUs.
 device_count = torch.cuda.device_count()
 compute_capabilities: Set[int] = set()
 for i in range(device_count):
@@ -37,12 +46,15 @@ for i in range(device_count):
     if major < 7:
         raise RuntimeError(
             "GPUs with compute capability less than 7.0 are not supported.")
-    compute_capabilities(major * 10 + minor)
+    compute_capabilities.add(major * 10 + minor)
+# If no GPU is available, add all supported compute capabilities.
 if not compute_capabilities:
     compute_capabilities = {70, 75, 80, 86, 90}
+# Add target compute capabilities to NVCC flags.
 for capability in compute_capabilities:
     NVCC_FLAGS += ["-gencode", f"arch=compute_{capability},code=sm_{capability}"]
 
+# Validate the NVCC CUDA version.
 nvcc_cuda_version = get_nvcc_cuda_version(CUDA_HOME)
 if nvcc_cuda_version < Version("11.0"):
     raise RuntimeError("CUDA 11.0 or higher is required to build the package.")
@@ -53,6 +65,7 @@ if 90 in compute_capabilities and nvcc_cuda_version < Version("11.8"):
     raise RuntimeError(
         "CUDA 11.8 or higher is required for GPUs with compute capability 9.0.")
 
+# Use NVCC threads to parallelize the build.
 if nvcc_cuda_version >= Version("11.2"):
     num_threads = min(os.cpu_count(), 8)
     NVCC_FLAGS += ["--threads", str(num_threads)]
@@ -95,7 +108,7 @@ ext_modules.append(layernorm_extension)
 activation_extension = CUDAExtension(
     name="aphrodite.activation_ops",
     sources=["kernels/activation.cpp", "kernels/activation_kernels.cu"],
-    extra_compile_flags={"cxx": CXX_FLAGS, "nvcc": NVCC_FLAGS},
+    extra_compile_args={"cxx": CXX_FLAGS, "nvcc": NVCC_FLAGS},
 )
 ext_modules.append(activation_extension)
 
@@ -105,51 +118,58 @@ def get_path(*filepath) -> str:
 
 
 def find_version(filepath: str):
+    """Extract version information from the given filepath.
+
+    Adapted from https://github.com/ray-project/ray/blob/0b190ee1160eeca9796bc091e07eaebf4c85b511/python/setup.py
+    """
     with open(filepath) as fp:
         version_match = re.search(
             r"^__version__ = ['\"]([^'\"]*)['\"]", fp.read(), re.M)
         if version_match:
             return version_match.group(1)
         raise RuntimeError("Unable to find version string.")
-    
+
 
 def read_readme() -> str:
+    """Read the README file."""
     return io.open(get_path("README.md"), "r", encoding="utf-8").read()
 
 
 def get_requirements() -> List[str]:
+    """Get Python package dependencies from requirements.txt."""
     with open(get_path("requirements.txt")) as f:
         requirements = f.read().strip().split("\n")
     return requirements
 
 
 setuptools.setup(
-    name="aphrodite",
-    version=find_version(get_path("aphrodite", "__init__.py")),
-    author="PygmalionAI Team",
-    license="AGPL-3.0",
-    description="The official inference engine for PygmalionAI",
+    name="aphrodite-engine",
+    version=find_version(get_path("vllm", "__init__.py")),
+    author="PygmalionAI",
+    license="AGPL 3.0",
+    description="The inference engine for PygmalionAI models",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/PygmalionAI/aphrodite-engine",
     project_urls={
-        "Homepage": "https://pygmalion.chat/",
-        "Documentation": "https://docs.pygmalion.chat/",
-        "HuggingFace": "https://huggingface.co/PygmalionAI",
+        "Homepage": "https://pygmalion.chat",
+        "Documentation": "https://docs.pygmalion.chat",
         "GitHub": "https://github.com/PygmalionAI",
+        "Huggingface": "https://huggingface.co/PygmalionAI",
     },
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: GNU AFFERO GENERAL PUBLIC LICENSE",
+        "License :: OSI Approved :: GNU Affero General Public License",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     packages=setuptools.find_packages(
-        exclude=("assets", "kernels", "examples")),
-    python_requirements=">=3.8",
+        exclude=("assets", "kernels","examples")),
+    python_requires=">=3.8",
     install_requires=get_requirements(),
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
 )
+
     
