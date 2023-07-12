138f1811ee302e1fdf240cfd5f4043862d0a6d70
AlpinDale <alpindale@gmail.com>
2023-07-12 18:38:33 +0000

    fix: missed an import

--- aphrodite/__init__.py
+++ aphrodite/__init__.py
@@ -2,6 +2,7 @@ from aphrodite.engine.args_tools import AsyncEngineArgs, EngineArgs
 from aphrodite.engine.async_aphrodite import AsyncAphrodite
 from aphrodite.engine.aphrodite_engine import AphroditeEngine
 from aphrodite.engine.ray_tools import initialize_cluster
+from aphrodite.endpoints.llm import LLM
 from aphrodite.common.outputs import CompletionOutput, RequestOutput
 from aphrodite.common.sampling_params import SamplingParams
 
