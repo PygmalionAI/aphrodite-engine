826de3ef93715e7600d9b10aa09c64939d44dab5
AlpinDale <alpindale@gmail.com>
2023-09-07 17:14:35 +0000

    use flash attention with xformers

--- aphrodite/modeling/layers/attention.py
+++ aphrodite/modeling/layers/attention.py
@@ -51,7 +51,6 @@ class PagedAttention(nn.Module):
         self.num_heads = num_heads
         self.head_size = head_size
         self.scale = float(scale)
-        self.attn_op = xops.fmha.cutlass.FwOp()
         self.num_kv_heads = num_heads if num_kv_heads is None else num_kv_heads
 
         assert self.num_heads % self.num_kv_heads == 0
@@ -105,7 +104,6 @@ class PagedAttention(nn.Module):
             attn_bias=input_metadata.attn_bias[0],
             p=0.0,
             scale=self.scale,
-            op=self.attn_op,
         )
         # TODO: Unnecessary copy. Optimize.
         output.copy_(out.squeeze(0))
@@ -379,7 +377,6 @@ class PagedAttentionWithALiBi(PagedAttention):
                 attn_bias=input_metadata.attn_bias[i],
                 p=0.0,
                 scale=self.scale,
-                op=self.attn_op,
             )
             # TODO: Unnecessary copy. Optimize.
             output[start:end].copy_(out.squeeze(0))
