a97a1fc2a1da2aed982d62fa26b3130ae91e2494
AlpinDale <alpindale@gmail.com>
2023-09-29 05:14:38 +0000

    false assertion

--- aphrodite/task_handler/worker.py
+++ aphrodite/task_handler/worker.py
@@ -156,7 +156,6 @@ class Worker:
         self,
         seq_group_metadata_list: List[SequenceGroupMetadata],
     ) -> Tuple[torch.Tensor, torch.Tensor, InputMetadata]:
-        assert self.block_size is not None
         seq_groups: List[Tuple[List[int], SamplingParams]] = []
         input_tokens: List[int] = []
         input_positions: List[int] = []
