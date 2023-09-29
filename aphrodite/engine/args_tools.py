8576f8c1f82202035db65d4c72b54ea40f8ceedc
AlpinDale <alpindale@gmail.com>
2023-09-29 03:35:29 +0000

    fix ctxlen issues with large prompts

--- aphrodite/engine/args_tools.py
+++ aphrodite/engine/args_tools.py
@@ -24,7 +24,7 @@ class EngineArgs:
     block_size: int = 16
     swap_space: int = 4  # GiB
     gpu_memory_utilization: float = 0.90
-    max_num_batched_tokens: int = 2560
+    max_num_batched_tokens: Optional[int] = None
     max_num_seqs: int = 256
     disable_log_stats: bool = False
     revision: Optional[str] = None
