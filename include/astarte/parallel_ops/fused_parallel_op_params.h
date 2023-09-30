7d20e4359dfac5b423852beeb6e48c0c046dbd69
AlpinDale <alpindale@gmail.com>
2023-09-30 03:11:57 +0000

    finished the fused parallel op (hash)

--- include/astarte/parallel_ops/fused_parallel_op_params.h
+++ include/astarte/parallel_ops/fused_parallel_op_params.h
@@ -1,5 +1,23 @@
 #ifndef _ASTARTE_FUSED_PARALLEL_OP_PARAMS_H
 #define _ASTARTE_FUSED_PARALLEL_OP_PARAMS_H
 
+#include "parallel_op_info.h"
+
+namespace astarte {
+
+struct FusedParallelOpParams {
+    std::vector<ParallelOpInfo> parallel_ops;
+    bool is_valid(ParallelTensorShape const &) const;
+};
+bool operator==(FusedParallelOpParams const &, FusedParallelOpParams const &);
+
+} // namespace astarte
+
+namespace std {
+template <>
+struct hash<astarte::FusedParallelOpParams> {
+    size_t operator()(astarte::FusedParallelOpParams const &) const;
+};
+} // namespace std
 
 #endif // _ASTARTE_FUSED_PARALLEL_OP_PARAMS_H
