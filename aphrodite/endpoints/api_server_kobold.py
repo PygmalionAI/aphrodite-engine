491a5fb3c4a12ca421e50ac79baacb32e338cb9f
g4rg <139017707+g4rg@users.noreply.github.com>
2023-10-02 14:03:41 +0000

    ignore use_default_badwordsid

--- aphrodite/endpoints/api_server_kobold.py
+++ aphrodite/endpoints/api_server_kobold.py
@@ -67,7 +67,7 @@ def prepare_engine_payload(kai_payload: KAIGenerationInputSchema) -> Tuple[Sampl
         top_a=kai_payload.top_a,
         typical_p=kai_payload.typical,
         stop=kai_payload.stop_sequence,
-        ignore_eos=kai_payload.use_default_badwordsids, # TODO ban instead
+        # ignore_eos=kai_payload.use_default_badwordsids, # TODO ban instead
         max_tokens=kai_payload.max_length,
     )
 
