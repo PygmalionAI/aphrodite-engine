2fba9ecd2499b1205d87f27ab3e89fb381f5ee10
g4rg <139017707+g4rg@users.noreply.github.com>
2023-10-02 19:28:47 +0000

    KAI: reduce schema

--- aphrodite/endpoints/protocol.py
+++ aphrodite/endpoints/protocol.py
@@ -30,38 +30,37 @@ class SamplingParams(BaseModel):
         return values
 
 class KAIGenerationInputSchema(BaseModel):
-    rep_pen: Optional[confloat(ge=1)] = Field(1.0, description="Base repetition penalty value.")
-    rep_pen_range: Optional[NonNegativeInt] = Field(description="Repetition penalty range.")
-    rep_pen_slope: Optional[NonNegativeFloat] = Field(description="Repetition penalty slope.")
-    top_k: Optional[NonNegativeInt] = Field(0.0, description="Top-k sampling value.")
-    top_a: Optional[NonNegativeFloat] = Field(0.0, description="Top-a sampling value.")
-    top_p: Optional[confloat(ge=0, le=1)] = Field(1.0, description="Top-p sampling value.")
-    tfs: Optional[confloat(ge=0, le=1)] = Field(1.0, description="Tail free sampling value.")
-    typical: Optional[confloat(ge=0, le=1)] = Field(1.0, description="Typical sampling value.")
-    temperature: Optional[PositiveFloat] = Field(1.0, description="Temperature value.")
-    prompt: str = Field(description="This is the submission.")
-    use_memory: Optional[bool] = Field(None, description="Whether or not to use the memory from the KoboldAI GUI when generating text.")
-    use_story: Optional[bool] = Field(None, description="Whether or not to use the story from the KoboldAI GUI when generating text.")
-    use_authors_note: Optional[bool] = Field(None, description="Whether or not to use the author's note from the KoboldAI GUI when generating text. This has no effect unless `use_story` is also enabled.")
-    use_world_info: Optional[bool] = Field(None, description="Whether or not to use the world info from the KoboldAI GUI when generating text.")
-    use_userscripts: Optional[bool] = Field(None, description="Whether or not to use the userscripts from the KoboldAI GUI when generating text.")
-    soft_prompt: Optional[str] = Field(None, description="Soft prompt to use when generating. If set to the empty string or any other string containing no non-whitespace characters, uses no soft prompt.")
-    max_length: PositiveInt = Field(description="Number of tokens to generate.")
-    max_context_length: PositiveInt = Field(description="Maximum number of tokens to send to the model.")
-    n: Optional[conint(ge=1, le=5)] = Field(1, description="Number of outputs to generate.")
-    disable_output_formatting: bool = Field(False, description="When enabled, all output formatting options default to `false` instead of the value in the KoboldAI GUI.")
-    frmttriminc: Optional[bool] = Field(description="Output formatting option. When enabled, removes some characters from the end of the output such that the output doesn't end in the middle of a sentence. If the output is less than one sentence long, does nothing.\n\nIf `disable_output_formatting` is `true`, this defaults to `false` instead of the value in the KoboldAI GUI.")
-    frmtrmblln: Optional[bool] = Field(description="Output formatting option. When enabled, replaces all occurrences of two or more consecutive newlines in the output with one newline.\n\nIf `disable_output_formatting` is `true`, this defaults to `false` instead of the value in the KoboldAI GUI.")
-    frmtrmspch: Optional[bool] = Field(description="Output formatting option. When enabled, removes `#/@%{}+=~|\^<>` from the output.\n\nIf `disable_output_formatting` is `true`, this defaults to `false` instead of the value in the KoboldAI GUI.")
-    singleline: Optional[bool] = Field(description="Output formatting option. When enabled, removes everything after the first line of the output, including the newline.\n\nIf `disable_output_formatting` is `true`, this defaults to `false` instead of the value in the KoboldAI GUI.")
-    use_default_badwordsids: Optional[bool] = Field(True, description="Ban tokens that commonly worsen the writing experience for continuous story writing")
-    disable_input_formatting: Optional[bool] = Field(description="When enabled, all input formatting options default to `false` instead of the value in the KoboldAI GUI")
-    frmtadsnsp: Optional[bool] = Field(description="Input formatting option. When enabled, adds a leading space to your input if there is no trailing whitespace at the end of the previous action.\n\nIf `disable_input_formatting` is `true`, this defaults to `false` instead of the value in the KoboldAI GUI.")
-    quiet: Optional[bool] = Field(description="When enabled, Generated output will not be displayed in the console.")
-    sampler_order: Optional[conlist(int, min_items=6)] = Field(description="Sampler order to be used. If N is the length of this array, then N must be greater than or equal to 6 and the array must be a permutation of the first N non-negative integers.")
-    sampler_seed: Optional[conint(ge=0, le=2**64 - 1)] = Field(description="RNG seed to use for sampling. If not specified, the global RNG will be used.")
-    sampler_full_determinism: Optional[bool] = Field(description="If enabled, the generated text will always be the same as long as you use the same RNG seed, input and settings. If disabled, only the *sequence* of generated texts that you get when repeatedly generating text will be the same given the same RNG seed, input and settings.")
-    stop_sequence: Optional[List[str]] = Field(description="An array of string sequences where the API will stop generating further tokens. The returned text WILL contain the stop sequence.")
+    prompt: str
+    n: Optional[conint(ge=1, le=5)] = 1
+    max_context_length: PositiveInt
+    max_length: PositiveInt
+    rep_pen: Optional[confloat(ge=1)] = 1.0
+    rep_pen_range: Optional[NonNegativeInt]
+    rep_pen_slope: Optional[NonNegativeFloat]
+    top_k: Optional[NonNegativeInt] = 0.0
+    top_a: Optional[NonNegativeFloat] = 0.0
+    top_p: Optional[confloat(ge=0, le=1)] = 1.0
+    tfs: Optional[confloat(ge=0, le=1)] = 1.0
+    typical: Optional[confloat(ge=0, le=1)] = 1.0
+    temperature: Optional[PositiveFloat] = 1.0
+    use_memory: Optional[bool]
+    use_story: Optional[bool]
+    use_authors_note: Optional[bool]
+    use_world_info: Optional[bool]
+    use_userscripts: Optional[bool]
+    soft_prompt: Optional[str]
+    disable_output_formatting: Optional[bool]
+    frmtrmblln: Optional[bool]
+    frmtrmspch: Optional[bool]
+    singleline: Optional[bool]
+    use_default_badwordsids: Optional[bool]
+    disable_input_formatting: Optional[bool]
+    frmtadsnsp: Optional[bool]
+    quiet: Optional[bool]
+    sampler_order: Optional[conlist(int, min_items=6)]
+    sampler_seed: Optional[conint(ge=0, le=2**64 - 1)]
+    sampler_full_determinism: Optional[bool]
+    stop_sequence: Optional[List[str]]
 
     @root_validator
     def check_context(cls, values):
