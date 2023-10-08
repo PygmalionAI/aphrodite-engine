78008197843509ebd0732fe6dba28a6642552d94
Henk <henk@henk.tech>
2023-10-09 00:02:36 +0200

    Linux runtime fixes

--- runtime.sh
+++ runtime.sh
@@ -2,4 +2,9 @@
 if [ ! -f "conda/envs/linux/bin/python" ]; then
 ./update-runtime.sh
 fi
+if [ $# -eq 0 ]
+  then
+    bin/micromamba run -r conda -n linux bash
+    exit
+fi
 bin/micromamba run -r conda -n linux $*
--- update-runtime.sh
+++ update-runtime.sh
@@ -5,4 +5,5 @@ if [ ! -f "conda/envs/linux/bin/python" ]; then
  bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
 fi
 bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
+bin/micromamba install -r conda -n linux gxx=10 -c conda-forge
 bin/micromamba run -r conda -n linux pip install -r requirements.txt
