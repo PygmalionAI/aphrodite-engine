46e472062a0883a16e8b9bb4728489cfdfe40da8
official-elinas <57051565+official-elinas@users.noreply.github.com>
2023-10-08 22:56:12 -0500

    chore: make NVCC work for different versions (#55)

--- update-runtime.sh
+++ update-runtime.sh
@@ -7,4 +7,6 @@ fi
 bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
 bin/micromamba install -r conda -n linux gxx=10 -c conda-forge -y
 bin/micromamba run -r conda -n linux pip install -r requirements.txt
+# Make it so the correct NVCC is found. Will do a quick search that shouldn't take more than a few seconds and get the first result.
+export CUDA_HOME=$(find / -type d -path "*/conda/envs/linux" 2>/dev/null | head -n 1)
 bin/micromamba run -r conda -n linux pip install -e .
