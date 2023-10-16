d327a598e3e77963f2c1c6de2f6280047973a998
50h100a <all_awful@proton.me>
2023-10-16 22:58:14 +0000

    don't prompt during update

--- update-runtime.sh
+++ update-runtime.sh
@@ -9,13 +9,5 @@ bin/micromamba install -r conda -n linux gxx=10 -c conda-forge -y
 bin/micromamba run -r conda -n linux pip install -r requirements.txt
 # Make it so the correct NVCC is found. Will do a quick search that shouldn't take more than a few seconds and get the first result.
 export CUDA_HOME=$(find / -type d -path "*/conda/envs/linux" 2>/dev/null | head -n 1)
-read -p "Do you want to install aphrodite from source? (y/n): " answer
-case ${answer:0:1} in
-    y|Y )
-        bin/micromamba run -r conda -n linux pip install -e .
-    ;;
-    * )
-        bin/micromamba run -r conda -n linux pip install aphrodite-engine
-    ;;
-esac
+bin/micromamba run -r conda -n linux pip install -e .
 
