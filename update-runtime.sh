1c605a8a2b8df114916ef05ccbe08f039f1202fe
AlpinDale <alpindale@gmail.com>
2023-10-28 17:51:41 +0000

    revert 'small changes'

--- update-runtime.sh
+++ update-runtime.sh
@@ -10,6 +10,6 @@ bin/micromamba run -r conda -n linux pip install -r requirements.txt
 # Make it so the correct NVCC is found. Looks only within the current working
 # directory, since find will return the *first* result, leading to conflicts
 # if you have multiple environments and one of them does not contain CUDA runtime.
-export CUDA_HOME=$(find . -type d -path "*/conda/envs/linux" 2>/dev/null | head -n 1)
+export CUDA_HOME=$(find / -type d -path "*/conda/envs/linux" 2>/dev/null | head -n 1)
 bin/micromamba run -r conda -n linux pip install -e .
 
