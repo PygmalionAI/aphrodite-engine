cff0048860443980eba1a8d59363c1644e581ea5
AlpinDale <alpindale@gmail.com>
2023-11-01 19:24:09 +0000

    fix compatibility layer

--- kernels/cuda_compat.h
+++ kernels/cuda_compat.h
@@ -1,22 +1,19 @@
 #pragma once
 
 #ifndef USE_ROCM
-    #define APHRODITE_LDG(arg) __ldg(arg)
+  #define APHRODITE_LDG(arg) __ldg(arg)
 #else
-    #define APHRODITE_LDG(arg) *(arg)
+  #define APHRODITE_LDG(arg) *(arg)
 #endif
 
 #ifndef USE_ROCM
-    #define APHRODITE_SHFL_XOR_SYNC(var, lane_mask)
-__shfl_xor_sync(uint32_t(-1), var, lane_mask)
+  #define APHRODITE_SHFL_XOR_SYNC(var, lane_mask) __shfl_xor_sync(uint32_t(-1), var, lane_mask)
 #else
-    #define APHRODITE_SHFL_XOR_SYNC(var, lane_mask)
-__shfl_xor(var, lane_mask)
+  #define APHRODITE_SHFL_XOR_SYNC(var, lane_mask) __shfl_xor(var, lane_mask)
 #endif
 
 #ifndef USE_ROCM
-    #define APHRODITE_SHFL_SYNC(var, src_lane)
-__shfl_sync(uint32_t(-1), var, src_lane)
+  #define APHRODITE_SHFL_SYNC(var, src_lane) __shfl_sync(uint32_t(-1), var, src_lane);
 #else
-    #define APHRODITE_SHFL_SYNC(var, src_lane) __shfl(var, src_lane)
+  #define APHRODITE_SHFL_SYNC(var, src_lane) __shfl(var, src_lane)
 #endif
