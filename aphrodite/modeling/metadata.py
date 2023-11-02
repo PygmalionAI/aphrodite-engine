272345fb217fb7d9def06a845b912174d5c97d6d
50h100a <all_awful@proton.me>
2023-11-02 23:04:34 +0000

    repr fix

--- aphrodite/modeling/metadata.py
+++ aphrodite/modeling/metadata.py
@@ -97,7 +97,8 @@ class InputMetadata:
                 f'prompt_lens={self.prompt_lens}, '
                 f'num_generation_tokens={self.num_generation_tokens}, '
                 f'context_lens={self.context_lens}, '
-                f'max_context_len={self.max_context_len}), '
+                f'max_context_len={self.max_context_len}, '
                 f'max_num_blocks_per_seq={self.max_num_blocks_per_seq}, '
-                f'block_tables={self.block_tables}), '
-                f'slot_mapping={self.slot_mapping}')
+                f'block_tables={self.block_tables}, '
+                f'slot_mapping={self.slot_mapping}, '
+                f'persistent_data={self.persistent_data})')
