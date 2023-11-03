6f472e06ee8ba760c37a1e79f55567b2c65dc123
AlpinDale <alpindale@gmail.com>
2023-11-03 16:32:57 +0000

    slightly bump version for pypi re-release

--- aphrodite/__init__.py
+++ aphrodite/__init__.py
@@ -6,7 +6,7 @@ from aphrodite.endpoints.llm import LLM
 from aphrodite.common.outputs import CompletionOutput, RequestOutput
 from aphrodite.common.sampling_params import SamplingParams
 
-__version__ = "0.4-dev"
+__version__ = "0.4.1-dev"
 
 __all__ = [
     "LLM",
