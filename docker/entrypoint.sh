b4d7c4a23bc59e950d2b86ebbf5513dd7ef6041f
Kristian Polso <kristian@polso.info>
2023-11-08 19:55:28 +0200

    Revert "Update entrypoint.sh"
    This reverts commit d9f0d762074d58209b9dcd2314d7d142d0e12616.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ source activate aphrodite-engine
 echo 'Starting Aphrodite Engine API server...'
 CMD="python -u -m aphrodite.endpoints.kobold.api_server \
              --host 0.0.0.0 \
-             --port 2242 \
+             --port 7860 \
              --model $MODEL_NAME \
              --tensor-parallel-size $NUM_GPUS \
              --dtype $DATATYPE"
