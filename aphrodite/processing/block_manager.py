a77557ce56a1e8b042a6be2f8164a49d5add7a80
AlpinDale <alpindale@gmail.com>
2023-11-30 01:30:16 +0000

    allocate status class

--- aphrodite/processing/block_manager.py
+++ aphrodite/processing/block_manager.py
@@ -1,4 +1,5 @@
 """A block manager that manages token blocks."""
+import enum
 from typing import Dict, List, Optional, Set, Tuple
 
 from aphrodite.common.block import PhysicalTokenBlock
@@ -53,6 +54,19 @@ class BlockAllocator:
 # Mapping: logical block number -> physical block.
 BlockTable = List[PhysicalTokenBlock]
 
+class AllocStatus(enum.Enum):
+    """Result for BlockSpaceManager.can_allocate().
+    
+    1. OK: seq_grouop can be allocated.
+    2. LATER: seq_group cannot be allocated.
+        The capacity of the allocator is larger
+        than seq_group required.
+    3. NEVER: seq_group can never be allocated.
+        The seq_group is too large to allocate in
+        GPU."""
+    OK    = enum.auto()
+    LATER = enum.auto()
+    NEVER = enum.auto()
 
 class BlockSpaceManager:
     """Manages the mapping between logical and physical token blocks."""
@@ -86,7 +100,7 @@ class BlockSpaceManager:
         # Mapping: seq_id -> BlockTable.
         self.block_tables: Dict[int, BlockTable] = {}
 
-    def can_allocate(self, seq_group: SequenceGroup) -> bool:
+    def can_allocate(self, seq_group: SequenceGroup) -> AllocStatus:
         # FIXME: Here we assume that all sequences in the group share
         # the same prompt. This may not be true for preempted sequences.
         seq = seq_group.get_seqs()[0]
@@ -96,8 +110,13 @@ class BlockSpaceManager:
                                       self.block_sliding_window)
         num_free_gpu_blocks = self.gpu_allocator.get_num_free_blocks()
         # Use watermark to avoid frequent cache eviction.
-        return (num_free_gpu_blocks - num_required_blocks >=
-                self.watermark_blocks)
+        if (self.num_total_gpu_blocks - num_required_blocks <
+                self.watermark_blocks):
+            return AllocStatus.NEVER
+        if num_free_gpu_blocks - num_required_blocks >= self.watermark_blocks:
+            return AllocStatus.OK
+        else:
+            return AllocStatus.LATER
 
     def allocate(self, seq_group: SequenceGroup) -> None:
         # NOTE: Here we assume that all sequences in the group have the same
