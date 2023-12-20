8bbf699f6f82ba13ca3617c2f3fb8d8f06962ba9
AlpinDale <alpindale@gmail.com>
2023-12-20 10:29:14 +0000

    enable pp from model loader (hopefully)

--- aphrodite/task_handler/worker.py
+++ aphrodite/task_handler/worker.py
@@ -9,7 +9,8 @@ from aphrodite.common.config import (CacheConfig, ModelConfig, ParallelConfig,
                                      SchedulerConfig)
 from aphrodite.modeling import set_random_seed
 from aphrodite.modeling.megatron.parallel_state import (
-    initialize_model_parallel)
+    initialize_model_parallel,
+    get_pipeline_model_parallel_rank)
 from aphrodite.common.sequence import SamplerOutput, SequenceGroupMetadata
 from aphrodite.task_handler.cache_engine import CacheEngine
 from aphrodite.task_handler.model_runner import ModelRunner
@@ -70,6 +71,7 @@ class Worker:
         # Initialize the distributed environment.
         _init_distributed_environment(self.parallel_config, self.rank,
                                       self.distributed_init_method)
+        self.pp_rank = get_pipeline_model_parallel_rank()
 
         # Initialize the model.
         set_random_seed(self.model_config.seed)
@@ -99,7 +101,8 @@ class Worker:
         peak_memory = total_gpu_memory - free_gpu_memory
 
         cache_block_size = CacheEngine.get_cache_block_size(
-            block_size, self.model_config, self.parallel_config)
+            block_size, self.model_config, self.parallel_config,
+            self.pp_rank)
         num_gpu_blocks = int(
             (total_gpu_memory * gpu_memory_utilization - peak_memory) //
             cache_block_size)
@@ -113,7 +116,7 @@ class Worker:
     def init_cache_engine(self, cache_config: CacheConfig) -> None:
         self.cache_config = cache_config
         self.cache_engine = CacheEngine(self.cache_config, self.model_config,
-                                        self.parallel_config)
+                                        self.parallel_config, self.pp_rank)
         self.cache_events = self.cache_engine.events
         self.gpu_cache = self.cache_engine.gpu_cache
         self.model_runner.set_block_size(self.cache_engine.block_size)
