833f2ce8efb574094319b6f3a658479f5a2d5254
AlpinDale <alpindale@gmail.com>
2023-12-22 21:28:43 +0000

    add cpu to dispatch utils

--- kernels/dispatch_utils.h
+++ kernels/dispatch_utils.h
@@ -12,3 +12,26 @@
 #define APHRODITE_DISPATCH_FLOATING_TYPES(TYPE, NAME, ...)             \
   AT_DISPATCH_SWITCH(                                             \
     TYPE, NAME, APHRODITE_DISPATCH_CASE_FLOATING_TYPES(__VA_ARGS__))
+
+#ifdef APHRODITE_BUILD_CPU_ONLY
+#define APHRODITE_DISPATCH_TO_CUDA_CASE(BASENAME, ...)
+#else
+#define APHRODITE_DISPATCH_TO_CUDA_CASE(BASENAME, ...)                  \
+  case c10::DeviceType::CUDA: {                                         \
+    return BASENAME##_cpu(__VA_ARGS__);                                 \
+  }
+#else
+#define APHRODITE_DISPATCH_TO_CPU_CASE(BASENAME, ...)
+#endif
+
+#define APHRODITE_DISPATCH_DEVICES(DEVICE, BASENAME, ...)               \
+  {                                                                     \
+    auto device = DEVICE.type();                                        \
+    switch (device) {                                                   \
+      APHRODITE_DISPATCH_TO_CPU_CASE(BASENAME, __VA_ARGS__)             \
+      APHRODITE_DISPATCH_TO_CUDA_CASE(BASENAME, __VA_ARGS__)            \
+    default:                                                            \
+      AT_ERROR('"', #BASENAME, "\" " not implemented for '"',           \
+               c10::DeviceTypeName(device), "'");                       \
+    }                                                                   \
+}
