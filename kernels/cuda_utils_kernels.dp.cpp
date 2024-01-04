e40c1610049c3534b86be3f277d023f5af96550f
AlpinDale <alpindale@gmail.com>
2024-01-03 22:58:31 -0500

    add sycl-modified kernels

---
+++ kernels/cuda_utils_kernels.dp.cpp
@@ -0,0 +1,14 @@
+#include <sycl/sycl.hpp>
+#include <dpct/dpct.hpp>
+int get_device_attribute(int attribute, int device_id)
+{
+    int device, value;
+    if (device_id < 0) {
+        device = dpct::dev_mgr::instance().current_device_id();
+    }
+    else {
+        device = device_id;
+    }
+
+    return dpct::dev_mgr::instance().get_device(device).get_local_mem_size();
+}
