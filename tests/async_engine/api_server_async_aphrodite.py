21cbf991752ea21ede64b8b9206d9ef85c6621af
AlpinDale <alpindale@gmail.com>
2024-01-14 05:30:36 +0000

    update async engine tests

--- tests/async_engine/api_server_async_aphrodite.py
+++ tests/async_engine/api_server_async_aphrodite.py
@@ -1,4 +1,4 @@
-"""API server with some extra logging for testing."""
+"""aphrodite.endpoints.ooba.api_server with some extra logging for testing."""
 import argparse
 from typing import Any, Dict
 
@@ -14,7 +14,6 @@ app = aphrodite.endpoints.ooba.api_server.app
 
 class AsyncAphroditeWithStats(AsyncAphrodite):
 
-    # pylint: disable=redefined-outer-name
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._num_aborts = 0
@@ -36,16 +35,17 @@ def stats() -> Response:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="localhost")
-    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--port", type=int, default=2242)
     parser = AsyncEngineArgs.add_cli_args(parser)
     args = parser.parse_args()
 
     engine_args = AsyncEngineArgs.from_cli_args(args)
     engine = AsyncAphroditeWithStats.from_engine_args(engine_args)
     aphrodite.endpoints.ooba.api_server.engine = engine
-    uvicorn.run(app,
-                host=args.host,
-                port=args.port,
-                log_level="debug",
-                timeout_keep_alive=aphrodite.endpoints.ooba.api_server.
-                TIMEOUT_KEEP_ALIVE)
+    uvicorn.run(
+        app,
+        host=args.host,
+        port=args.port,
+        log_level="debug",
+        timeout_keep_alive=aphrodite.endpoints.ooba.api_server.TIMEOUT_KEEP_ALIVE)
+    
