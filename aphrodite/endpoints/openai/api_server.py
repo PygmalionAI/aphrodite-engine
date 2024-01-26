604f675600a5a94eacde98fee6f53155589d6515
AlpinDale <alpindale@gmail.com>
2024-01-26 06:34:04 +0000

    fix: logprobs at -inf

--- aphrodite/endpoints/openai/api_server.py
+++ aphrodite/endpoints/openai/api_server.py
@@ -262,6 +262,12 @@ def create_logprobs(
                 tokenizer.convert_ids_to_tokens(i): p
                 for i, p in step_top_logprobs.items()
             } if step_top_logprobs else None)
+
+    logprobs.top_logprobs = [
+        {k: v if v > -1000 else -1000 for k, v in top_logprob.items()}
+        for top_logprob in logprobs.top_logprobs if top_logprob is not None
+    ]
+
     return logprobs
 
 
