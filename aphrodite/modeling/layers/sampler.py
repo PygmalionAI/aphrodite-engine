2e3318c1fa9097321e138028694801d5cbc6ab80
50h100a <all_awful@proton.me>
2024-02-03 02:02:00 +0000

    yapf considers this space to be CRITICAL

--- aphrodite/modeling/layers/sampler.py
+++ aphrodite/modeling/layers/sampler.py
@@ -258,7 +258,7 @@ def _apply_alphabet_soup(
     logits_sort[mask] = -float("inf")
 
     # Apply top-k.
-    for i,topk in enumerate(k):
+    for i, topk in enumerate(k):
         logits_sort[i, topk:] = -float("inf")
 
     # Re-sort the probabilities.
