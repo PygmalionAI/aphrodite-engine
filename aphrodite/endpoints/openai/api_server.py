1c7016bb1d952550dc93698c79e9b056c6e0c61b
AlpinDale <alpindale@gmail.com>
2024-02-18 11:30:16 +0000

    fix case when api key is none

--- aphrodite/endpoints/openai/api_server.py
+++ aphrodite/endpoints/openai/api_server.py
@@ -133,7 +133,7 @@ app.mount("/metrics", metrics_app)
 
 async def get_api_key(api_key_header: str = Depends(api_key_header)):
     api_key = os.environ.get("APHRODITE_API_KEY") or args.api_keys
-    if api_key_header != "Bearer " + api_key:
+    if api_key is not None and api_key_header != "Bearer " + api_key:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Invalid API Key")
     return api_key_header
