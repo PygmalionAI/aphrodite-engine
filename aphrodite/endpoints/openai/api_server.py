f97ec78d55d1d8ff274de03cee804178755ce5e0
AlpinDale <alpindale@gmail.com>
2024-03-03 15:32:49 +0000

    merge kobold into openai again

--- aphrodite/endpoints/openai/api_server.py
+++ aphrodite/endpoints/openai/api_server.py
@@ -5,31 +5,40 @@ from contextlib import asynccontextmanager
 import os
 import importlib
 import inspect
-from typing import List
+from typing import List, Tuple, AsyncGenerator
 
 from prometheus_client import make_asgi_app
 import fastapi
 import uvicorn
 from http import HTTPStatus
-from fastapi import Request
+from fastapi import Request, APIRouter
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse, StreamingResponse, Response
+from fastapi.responses import JSONResponse, StreamingResponse, Response, HTMLResponse
 
 from aphrodite.engine.args_tools import AsyncEngineArgs
 from aphrodite.engine.async_aphrodite import AsyncAphrodite
 from aphrodite.endpoints.openai.protocol import (
     CompletionRequest, ChatCompletionRequest, ErrorResponse, Prompt)
 from aphrodite.common.logger import init_logger
+from aphrodite.common.outputs import RequestOutput
+from aphrodite.common.sampling_params import SamplingParams, _SAMPLING_EPS
+from aphrodite.common.utils import random_uuid
 from aphrodite.endpoints.openai.serving_chat import OpenAIServingChat
 from aphrodite.endpoints.openai.serving_completions import OpenAIServingCompletion
+from aphrodite.endpoints.openai.protocol import KAIGenerationInputSchema
 from aphrodite.endpoints.openai.serving_engine import LoRA
+from aphrodite.transformers_utils.tokenizer import get_tokenizer
 
 TIMEOUT_KEEP_ALIVE = 5  # seconds
 
 openai_serving_chat: OpenAIServingChat = None
 openai_serving_completion: OpenAIServingCompletion = None
 logger = init_logger(__name__)
+kai_api = APIRouter()
+extra_api = APIRouter()
+kobold_lite_ui = ""
+gen_cache: dict = {}
 
 
 @asynccontextmanager
@@ -86,6 +95,16 @@ def parse_args():
         help=
         "If provided, the server will require this key to be presented in the header."
     )
+    parser.add_argument(
+        "--launch-kobold-api",
+        action="store_true",
+        help="Launch the Kobold API server in addition to the OpenAI API server.")
+    parser.add_argument(
+        "--max-length",
+        type=int,
+        default=256,
+        help="The maximum length of the generated response. "
+        "For use with Kobold Horde.")
     parser.add_argument("--served-model-name",
                         type=str,
                         default=None,
@@ -205,9 +224,228 @@ async def create_completion(request: CompletionRequest, raw_request: Request):
     else:
         return JSONResponse(content=generator.model_dump())
 
+# ============ KoboldAI API ============ #
+
+def _set_badwords(tokenizer, hf_config):  # pylint: disable=redefined-outer-name
+    global badwordsids
+    if hf_config.bad_words_ids is not None:
+        badwordsids = hf_config.bad_words_ids
+        return
+
+    badwordsids = [
+        v for k, v in tokenizer.get_vocab().items()
+        if any(c in str(k) for c in "[]")
+    ]
+    if tokenizer.pad_token_id in badwordsids:
+        badwordsids.remove(tokenizer.pad_token_id)
+    badwordsids.append(tokenizer.eos_token_id)
+
+def prepare_engine_payload(
+        kai_payload: KAIGenerationInputSchema
+) -> Tuple[SamplingParams, List[int]]:
+    """Create SamplingParams and truncated input tokens for AsyncEngine"""
+
+    if not kai_payload.genkey:
+        kai_payload.genkey = f"kai-{random_uuid()}"
+
+    # if kai_payload.max_context_length > engine_args.max_model_len:
+    #     raise ValueError(
+    #         f"max_context_length ({kai_payload.max_context_length}) "
+    #         "must be less than or equal to "
+    #         f"max_model_len ({engine_args.max_model_len})")
+
+    kai_payload.top_k = kai_payload.top_k if kai_payload.top_k != 0.0 else -1
+    kai_payload.tfs = max(_SAMPLING_EPS, kai_payload.tfs)
+    if kai_payload.temperature < _SAMPLING_EPS:
+        kai_payload.n = 1
+        kai_payload.top_p = 1.0
+        kai_payload.top_k = -1
+
+    sampling_params = SamplingParams(
+        n=kai_payload.n,
+        best_of=kai_payload.n,
+        repetition_penalty=kai_payload.rep_pen,
+        temperature=kai_payload.temperature,
+        dynatemp_range=kai_payload.dynatemp_range,
+        dynatemp_exponent=kai_payload.dynatemp_exponent,
+        smoothing_factor=kai_payload.smoothing_factor,
+        tfs=kai_payload.tfs,
+        top_p=kai_payload.top_p,
+        top_k=kai_payload.top_k,
+        top_a=kai_payload.top_a,
+        min_p=kai_payload.min_p,
+        typical_p=kai_payload.typical,
+        eta_cutoff=kai_payload.eta_cutoff,
+        epsilon_cutoff=kai_payload.eps_cutoff,
+        mirostat_mode=kai_payload.mirostat,
+        mirostat_tau=kai_payload.mirostat_tau,
+        mirostat_eta=kai_payload.mirostat_eta,
+        stop=kai_payload.stop_sequence,
+        include_stop_str_in_output=kai_payload.include_stop_str_in_output,
+        custom_token_bans=badwordsids
+        if kai_payload.use_default_badwordsids else [],
+        max_tokens=kai_payload.max_length,
+    )
+
+    max_input_tokens = max(
+        1, kai_payload.max_context_length - kai_payload.max_length)
+    input_tokens = tokenizer(kai_payload.prompt).input_ids[-max_input_tokens:]
+
+    return sampling_params, input_tokens
+
+@kai_api.post("/generate")
+async def generate(kai_payload: KAIGenerationInputSchema) -> JSONResponse:
+    sampling_params, input_tokens = prepare_engine_payload(kai_payload)
+    result_generator = engine.generate(None, sampling_params,
+                                       kai_payload.genkey, input_tokens)
+
+    final_res: RequestOutput = None
+    previous_output = ""
+    async for res in result_generator:
+        final_res = res
+        new_chunk = res.outputs[0].text[len(previous_output):]
+        previous_output += new_chunk
+        gen_cache[kai_payload.genkey] = previous_output
+
+    assert final_res is not None
+    del gen_cache[kai_payload.genkey]
+
+    return JSONResponse(
+        {"results": [{
+            "text": output.text
+        } for output in final_res.outputs]})
+
+@extra_api.post("/generate/stream")
+async def generate_stream(
+    kai_payload: KAIGenerationInputSchema) -> StreamingResponse:
+
+    sampling_params, input_tokens = prepare_engine_payload(kai_payload)
+    results_generator = engine.generate(None, sampling_params,
+                                        kai_payload.genkey, input_tokens)
+
+    async def stream_kobold() -> AsyncGenerator[bytes, None]:
+        previous_output = ""
+        async for res in results_generator:
+            new_chunk = res.outputs[0].text[len(previous_output):]
+            previous_output += new_chunk
+            yield b"event: message\n"
+            yield f"data: {json.dumps({'token': new_chunk})}\n\n".encode()
+
+    return StreamingResponse(stream_kobold(),
+                             headers={
+                                 "Cache-Control": "no-cache",
+                                 "Connection": "keep-alive",
+                             },
+                             media_type="text/event-stream")
+
+@extra_api.post("/generate/check")
+@extra_api.get("/generate/check")
+async def check_generation(request: Request):
+    text = ""
+    try:
+        request_dict = await request.json()
+        if "genkey" in request_dict and request_dict["genkey"] in gen_cache:
+            text = gen_cache[request_dict["genkey"]]
+    except json.JSONDecodeError:
+        pass
+
+    return JSONResponse({"results": [{"text": text}]})
+
+
+@extra_api.post("/abort")
+async def abort_generation(request: Request):
+    try:
+        request_dict = await request.json()
+        if "genkey" in request_dict:
+            await engine.abort(request_dict["genkey"])
+    except json.JSONDecodeError:
+        pass
+
+    return JSONResponse({})
+
+@extra_api.post("/tokencount")
+async def count_tokens(request: Request):
+    """Tokenize string and return token count"""
+
+    request_dict = await request.json()
+    tokenizer_result = await openai_serving_chat.tokenize(request_dict["prompt"])
+    return JSONResponse({"value": len(tokenizer_result)})
+
+@kai_api.get("/info/version")
+async def get_version():
+    """Impersonate KAI"""
+    return JSONResponse({"result": "1.2.4"})
+
+@kai_api.get("/model")
+async def get_model():
+    return JSONResponse({"result": f"aphrodite/{served_model}"})
+
+@kai_api.get("/config/soft_prompts_list")
+async def get_available_softprompts():
+    """Stub for compatibility"""
+    return JSONResponse({"values": []})
+
+
+@kai_api.get("/config/soft_prompt")
+async def get_current_softprompt():
+    """Stub for compatibility"""
+    return JSONResponse({"value": ""})
+
+
+@kai_api.put("/config/soft_prompt")
+async def set_current_softprompt():
+    """Stub for compatibility"""
+    return JSONResponse({})
+
+@kai_api.get("/config/max_length")
+async def get_max_length() -> JSONResponse:
+    max_length = args.max_length
+    return JSONResponse({"value": max_length})
+
+@kai_api.get("/config/max_context_length")
+@extra_api.get("/true_max_context_length")
+async def get_max_context_length() -> JSONResponse:
+    max_context_length = engine_args.max_model_len
+    return JSONResponse({"value": max_context_length})
+
+@extra_api.get("/preloadstory")
+async def get_preloaded_story() -> JSONResponse:
+    """Stub for compatibility"""
+    return JSONResponse({})
+
+
+@extra_api.get("/version")
+async def get_extra_version():
+    """Impersonate KoboldCpp"""
+    return JSONResponse({"result": "KoboldCpp", "version": "1.55.1"})
+
+@app.get("/")
+async def get_kobold_lite_ui():
+    """Serves a cached copy of the Kobold Lite UI, loading it from disk
+    on demand if needed."""
+    global kobold_lite_ui
+    if kobold_lite_ui == "":
+        scriptpath = os.path.dirname(os.path.abspath(__file__))
+        klitepath = os.path.join(scriptpath, "../kobold/klite.embd")
+        klitepath = os.path.normpath(klitepath)  # Normalize the path
+        if os.path.exists(klitepath):
+            with open(klitepath, "r") as f:
+                kobold_lite_ui = f.read()
+        else:
+            logger.error("Kobold Lite UI not found at " + klitepath)
+    return HTMLResponse(content=kobold_lite_ui)
+# ============ KoboldAI API ============ #
 
 if __name__ == "__main__":
     args = parse_args()
+
+    if 'launch_kobold_api' in args:
+        logger.warning("Launching Kobold API server in addition to OpenAI. "
+                       "Keep in mind that the Kobold API routes are NOT "
+                       "protected via the API key.")
+        app.include_router(kai_api, prefix="/api/v1")
+        app.include_router(kai_api, prefix="/api/latest", include_in_schema=False)
+        app.include_router(extra_api, prefix="/api/extra")
 
     app.add_middleware(
         CORSMiddleware,
@@ -221,6 +459,9 @@ if __name__ == "__main__":
 
         @app.middleware("http")
         async def authentication(request: Request, call_next):
+            excluded_paths = ["/api"]
+            if any(request.url.path.startswith(path) for path in excluded_paths):
+                return await call_next(request)
             if not request.url.path.startswith("/v1"):
                 return await call_next(request)
 
@@ -259,6 +500,13 @@ if __name__ == "__main__":
                                             args.chat_template)
     openai_serving_completion = OpenAIServingCompletion(
         engine, served_model, args.lora_modules)
+    engine_model_config = asyncio.run(engine.get_model_config())
+    tokenizer = get_tokenizer(engine_args.tokenizer,
+                              tokenizer_mode=engine_args.tokenizer_mode,
+                              trust_remote_code=engine_args.trust_remote_code,)
+
+    if 'launch_kobold_api' in args:
+        _set_badwords(tokenizer, engine_model_config.hf_config)
 
     app.root_path = args.root_path
     uvicorn.run(app,
--- aphrodite/endpoints/openai/protocol.py
+++ aphrodite/endpoints/openai/protocol.py
@@ -3,7 +3,7 @@
 import time
 from typing import Dict, List, Literal, Optional, Union
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, root_validator
 import torch
 
 from aphrodite.common.utils import random_uuid
@@ -367,3 +367,95 @@ class ChatCompletionStreamResponse(BaseModel):
 
 class Prompt(BaseModel):
     prompt: str
+
+
+# ========== KoboldAI ========== #
+
+class KoboldSamplingParams(BaseModel):
+    n: int = Field(1, alias="n")
+    best_of: Optional[int] = Field(None, alias="best_of")
+    presence_penalty: float = Field(0.0, alias="presence_penalty")
+    frequency_penalty: float = Field(0.0, alias="rep_pen")
+    temperature: float = Field(1.0, alias="temperature")
+    dynatemp_range: Optional[float] = 0.0
+    dynatemp_exponent: Optional[float] = 1.0
+    smoothing_factor: Optional[float] = 0.0
+    top_p: float = Field(1.0, alias="top_p")
+    top_k: float = Field(-1, alias="top_k")
+    min_p: float = Field(0.0, alias="min_p")
+    top_a: float = Field(0.0, alias="top_a")
+    tfs: float = Field(1.0, alias="tfs")
+    eta_cutoff: float = Field(0.0, alias="eta_cutoff")
+    epsilon_cutoff: float = Field(0.0, alias="epsilon_cutoff")
+    typical_p: float = Field(1.0, alias="typical_p")
+    use_beam_search: bool = Field(False, alias="use_beam_search")
+    length_penalty: float = Field(1.0, alias="length_penalty")
+    early_stopping: Union[bool, str] = Field(False, alias="early_stopping")
+    stop: Union[None, str, List[str]] = Field(None, alias="stop_sequence")
+    include_stop_str_in_output: Optional[bool] = False
+    ignore_eos: bool = Field(False, alias="ignore_eos")
+    max_tokens: int = Field(16, alias="max_length")
+    logprobs: Optional[int] = Field(None, alias="logprobs")
+    custom_token_bans: Optional[List[int]] = Field(None,
+                                                   alias="custom_token_bans")
+
+    @root_validator(pre=False, skip_on_failure=True)
+    def validate_best_of(cls, values):  # pylint: disable=no-self-argument
+        best_of = values.get("best_of")
+        n = values.get("n")
+        if best_of is not None and (best_of <= 0 or best_of > n):
+            raise ValueError(
+                "best_of must be a positive integer less than or equal to n")
+        return values    
+
+class KAIGenerationInputSchema(BaseModel):
+    genkey: Optional[str] = None
+    prompt: str
+    n: Optional[int] = 1
+    max_context_length: int
+    max_length: int
+    rep_pen: Optional[float] = 1.0
+    rep_pen_range: Optional[int]
+    rep_pen_slope: Optional[float]
+    top_k: Optional[int] = 0
+    top_a: Optional[float] = 0.0
+    top_p: Optional[float] = 1.0
+    min_p: Optional[float] = 0.0
+    tfs: Optional[float] = 1.0
+    eps_cutoff: Optional[float] = 0.0
+    eta_cutoff: Optional[float] = 0.0
+    typical: Optional[float] = 1.0
+    temperature: Optional[float] = 1.0
+    dynatemp_range: Optional[float] = 0.0
+    dynatemp_exponent: Optional[float] = 1.0
+    smoothing_factor: Optional[float] = 0.0
+    use_memory: Optional[bool] = None
+    use_story: Optional[bool] = None
+    use_authors_note: Optional[bool] = None
+    use_world_info: Optional[bool] = None
+    use_userscripts: Optional[bool] = None
+    soft_prompt: Optional[str] = None
+    disable_output_formatting: Optional[bool] = None
+    frmtrmblln: Optional[bool] = None
+    frmtrmspch: Optional[bool] = None
+    singleline: Optional[bool] = None
+    use_default_badwordsids: Optional[bool] = None
+    mirostat: Optional[int] = 0
+    mirostat_tau: Optional[float] = 0.0
+    mirostat_eta: Optional[float] = 0.0
+    disable_input_formatting: Optional[bool] = None
+    frmtadsnsp: Optional[bool] = None
+    quiet: Optional[bool] = None
+    # pylint: disable=unexpected-keyword-arg
+    sampler_order: Optional[Union[List, str]] = Field(default_factory=list)
+    sampler_seed: Optional[int] = None
+    sampler_full_determinism: Optional[bool] = None
+    stop_sequence: Optional[List[str]] = None
+    include_stop_str_in_output: Optional[bool] = False
+
+    @root_validator(pre=False, skip_on_failure=True)
+    def check_context(cls, values):  # pylint: disable=no-self-argument
+        assert values.get("max_length") <= values.get(
+            "max_context_length"
+        ), "max_length must not be larger than max_context_length"
+        return values
