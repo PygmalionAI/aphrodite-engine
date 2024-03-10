958c382c2c7b3c8ca15698431b691e168bb2aec5
Light <sgsdxzy@gmail.com>
2024-03-10 14:21:10 +0800

    Fix typo in marlin kernel path.

--- setup.py
+++ setup.py
@@ -313,7 +313,7 @@ aphrodite_extension_sources = [
 if _is_cuda():
     aphrodite_extension_sources.append("kernels/quantization/awq/gemm_kernels.cu")
     aphrodite_extension_sources.append("kernels/quantization/quip/origin_order.cu")
-    aphrodite_extension_sources.append("kernels/quanhtization/marlin/marlin_cuda_kernel.cu")
+    aphrodite_extension_sources.append("kernels/quantization/marlin/marlin_cuda_kernel.cu")
     aphrodite_extension_sources.append("kernels/all_reduce/custom_all_reduce.cu")
     aphrodite_extension_sources.append("kernels/quantization/aqlm/aqlm_cuda_entry.cpp")
     aphrodite_extension_sources.append("kernels/quantization/aqlm/aqlm_cuda_kernel.cu")
