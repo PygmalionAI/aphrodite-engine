e2a7b50440dcffd863bb39ad14e0ae41b89f258e
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2024-03-15 20:37:42 +0000

    fix: logprobs when inf or nan (#329)
    * fix logprobs when inf or nan
    
    * yapf

--- aphrodite/endpoints/openai/serving_engine.py
+++ aphrodite/endpoints/openai/serving_engine.py
@@ -126,6 +126,12 @@ class OpenAIServing:
                     p.decoded_token: p.logprob
                     for i, p in step_top_logprobs.items()
                 } if step_top_logprobs else None)
+
+        logprobs.top_logprobs = [{
+            k: v if v > -1000 else -1000
+            for k, v in top_logprob.items()
+        } for top_logprob in logprobs.top_logprobs if top_logprob is not None]
+
         return logprobs
 
     def create_error_response(
