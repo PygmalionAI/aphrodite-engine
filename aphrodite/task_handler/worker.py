e3f598390ee820bb894681803c53ce165c881a93
50h100a <all_awful@proton.me>
2024-03-16 21:05:40 +0000

    yapf. >:(

--- aphrodite/task_handler/worker.py
+++ aphrodite/task_handler/worker.py
@@ -145,7 +145,7 @@ class Worker:
 
         cache_block_size = CacheEngine.get_cache_block_size(
             block_size, cache_dtype, self.model_config, self.parallel_config)
-        num_gpu_blocks = total_kvcache_used//block_size + int(
+        num_gpu_blocks = total_kvcache_used // block_size + int(
             (total_gpu_memory * gpu_memory_utilization - peak_memory) //
             cache_block_size)
         num_cpu_blocks = int(cpu_swap_space // cache_block_size)
