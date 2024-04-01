7a07ccae328c23b66c22f0b2579078e3ad1cea28
AlpinDale <alpindale@gmail.com>
2024-04-01 16:01:49 +0000

    add cache config to metrics

--- aphrodite/engine/metrics.py
+++ aphrodite/engine/metrics.py
@@ -1,10 +1,17 @@
+from loguru import logger
+from prometheus_client import (
+    Counter,
+    Gauge,
+    Histogram,
+    Info,
+    REGISTRY,
+    disable_created_metrics,
+)
+
 import time
 import numpy as np
 from typing import Dict, List
 from dataclasses import dataclass
-
-from prometheus_client import Counter, Gauge, Histogram, disable_created_metrics
-from loguru import logger
 
 disable_created_metrics()
 
@@ -14,60 +21,105 @@ disable_created_metrics()
 
 # begin-metrics-definitions
 class Metrics:
-
     def __init__(self, labelnames: List[str]):
+        # Unregister any existing Aphrodite collectors
+        for collector in list(REGISTRY._collector_to_names):
+            if hasattr(collector, "_name") and "aphrodite" in collector._name:
+                REGISTRY.unregister(collector)
+
+        # Config Information
+        self.info_cache_config = Info(
+            name="aphrodite:cache_config",
+            documentation="information of cache_config",
+        )
+
         # System stats
         self.gauge_scheduler_running = Gauge(
             name="aphrodite:num_requests_running",
             documentation="Number of requests currently running on GPU.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
         self.gauge_scheduler_swapped = Gauge(
             name="aphrodite:num_requests_swapped",
             documentation="Number of requests swapped to CPU.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
         self.gauge_scheduler_waiting = Gauge(
             name="aphrodite:num_requests_waiting",
             documentation="Number of requests waiting to be processed.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
         self.gauge_gpu_cache_usage = Gauge(
             name="aphrodite:gpu_cache_usage_perc",
             documentation="GPU KV-cache usage. 1 means 100 percent usage.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
         self.gauge_cpu_cache_usage = Gauge(
             name="aphrodite:cpu_cache_usage_perc",
             documentation="CPU KV-cache usage. 1 means 100 percent usage.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
 
         # Raw stats from last model iteration
         self.counter_prompt_tokens = Counter(
             name="aphrodite:prompt_tokens_total",
             documentation="Number of prefill tokens processed.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
         self.counter_generation_tokens = Counter(
             name="aphrodite:generation_tokens_total",
             documentation="Number of generation tokens processed.",
-            labelnames=labelnames)
+            labelnames=labelnames,
+        )
         self.histogram_time_to_first_token = Histogram(
             name="aphrodite:time_to_first_token_seconds",
             documentation="Histogram of time to first token in seconds.",
             labelnames=labelnames,
             buckets=[
-                0.001, 0.005, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.25, 0.5,
-                0.75, 1.0, 2.5, 5.0, 7.5, 10.0
-            ])
+                0.001,
+                0.005,
+                0.01,
+                0.02,
+                0.04,
+                0.06,
+                0.08,
+                0.1,
+                0.25,
+                0.5,
+                0.75,
+                1.0,
+                2.5,
+                5.0,
+                7.5,
+                10.0,
+            ],
+        )
         self.histogram_time_per_output_token = Histogram(
             name="aphrodite:time_per_output_token_seconds",
             documentation="Histogram of time per output token in seconds.",
             labelnames=labelnames,
             buckets=[
-                0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.75,
-                1.0, 2.5
-            ])
+                0.01,
+                0.025,
+                0.05,
+                0.075,
+                0.1,
+                0.15,
+                0.2,
+                0.3,
+                0.4,
+                0.5,
+                0.75,
+                1.0,
+                2.5,
+            ],
+        )
         self.histogram_e2e_request_latency = Histogram(
             name="aphrodite:e2e_request_latency_seconds",
             documentation="Histogram of end to end request latency in seconds.",
             labelnames=labelnames,
-            buckets=[1.0, 2.5, 5.0, 10.0, 15.0, 20.0, 30.0, 40.0, 50.0, 60.0])
+            buckets=[1.0, 2.5, 5.0, 10.0, 15.0, 20.0, 30.0, 40.0, 50.0, 60.0],
+        )
 
         # Legacy metrics
         self.gauge_avg_prompt_throughput = Gauge(
@@ -88,6 +140,7 @@ class Metrics:
 @dataclass
 class Stats:
     """Created by AphroditeEngine for use by StatLogger."""
+
     now: float
 
     # System stats.
@@ -121,6 +174,10 @@ class StatLogger:
         self.labels = labels
         self.metrics = Metrics(labelnames=list(labels.keys()))
 
+    def info(self, type: str, obj: object) -> None:
+        if type == "cache_config":
+            self.metrics.info_cache_config.info(obj.metrics_info())
+
     def _get_throughput(self, tracked_stats: List[int], now: float) -> float:
         return float(np.sum(tracked_stats) / (now - self.last_local_log))
 
@@ -131,35 +188,46 @@ class StatLogger:
     def _log_prometheus(self, stats: Stats) -> None:
         # Set system stat gauges.
         self.metrics.gauge_scheduler_running.labels(**self.labels).set(
-            stats.num_running)
+            stats.num_running
+        )
         self.metrics.gauge_scheduler_swapped.labels(**self.labels).set(
-            stats.num_swapped)
+            stats.num_swapped
+        )
         self.metrics.gauge_scheduler_waiting.labels(**self.labels).set(
-            stats.num_waiting)
+            stats.num_waiting
+        )
         self.metrics.gauge_gpu_cache_usage.labels(**self.labels).set(
-            stats.gpu_cache_usage)
+            stats.gpu_cache_usage
+        )
         self.metrics.gauge_cpu_cache_usage.labels(**self.labels).set(
-            stats.cpu_cache_usage)
+            stats.cpu_cache_usage
+        )
 
         # Add to token counters.
         self.metrics.counter_prompt_tokens.labels(**self.labels).inc(
-            stats.num_prompt_tokens)
+            stats.num_prompt_tokens
+        )
         self.metrics.counter_generation_tokens.labels(**self.labels).inc(
-            stats.num_generation_tokens)
+            stats.num_generation_tokens
+        )
 
         # Observe request level latencies in histograms.
         for ttft in stats.time_to_first_tokens:
             self.metrics.histogram_time_to_first_token.labels(
-                **self.labels).observe(ttft)
+                **self.labels
+            ).observe(ttft)
         for tpot in stats.time_per_output_tokens:
             self.metrics.histogram_time_per_output_token.labels(
-                **self.labels).observe(tpot)
+                **self.labels
+            ).observe(tpot)
         for e2e in stats.time_e2e_requests:
             self.metrics.histogram_e2e_request_latency.labels(
-                **self.labels).observe(e2e)
+                **self.labels
+            ).observe(e2e)
 
-    def _log_prometheus_interval(self, prompt_throughput: float,
-                                 generation_throughput: float) -> None:
+    def _log_prometheus_interval(
+        self, prompt_throughput: float, generation_throughput: float
+    ) -> None:
         # Logs metrics to prometheus that are computed every logging_interval.
         # Support legacy gauge metrics that make throughput calculations on
         # the Aphrodite side.
@@ -167,15 +235,17 @@ class StatLogger:
         # counter_generation_tokens
         # Which log raw data and calculate summaries using rate() on the
         # grafana/prometheus side.
-        self.metrics.gauge_avg_prompt_throughput.labels(
-            **self.labels).set(prompt_throughput)
-        self.metrics.gauge_avg_generation_throughput.labels(
-            **self.labels).set(generation_throughput)
+        self.metrics.gauge_avg_prompt_throughput.labels(**self.labels).set(
+            prompt_throughput
+        )
+        self.metrics.gauge_avg_generation_throughput.labels(**self.labels).set(
+            generation_throughput
+        )
 
     def log(self, stats: Stats) -> None:
         """Called by AphroditeEngine.
-           Logs to prometheus and tracked stats every iteration.
-           Logs to Stdout every self.local_interval seconds."""
+        Logs to prometheus and tracked stats every iteration.
+        Logs to Stdout every self.local_interval seconds."""
 
         # Log to prometheus.
         self._log_prometheus(stats)
@@ -186,16 +256,18 @@ class StatLogger:
 
         # Log locally every local_interval seconds.
         if self._local_interval_elapsed(stats.now):
-
             # Compute summary metrics for tracked stats (and log them to
             # prometheus if applicable).
-            prompt_throughput = self._get_throughput(self.num_prompt_tokens,
-                                                     now=stats.now)
+            prompt_throughput = self._get_throughput(
+                self.num_prompt_tokens, now=stats.now
+            )
             generation_throughput = self._get_throughput(
-                self.num_generation_tokens, now=stats.now)
+                self.num_generation_tokens, now=stats.now
+            )
             self._log_prometheus_interval(
                 prompt_throughput=prompt_throughput,
-                generation_throughput=generation_throughput)
+                generation_throughput=generation_throughput,
+            )
 
             # Log to stdout.
             logger.info(
@@ -206,7 +278,8 @@ class StatLogger:
                 f"Swapped: {stats.num_swapped} reqs, "
                 f"Pending: {stats.num_waiting} reqs, "
                 f"GPU KV cache usage: {stats.gpu_cache_usage * 100:.1f}%, "
-                f"CPU KV cache usage: {stats.cpu_cache_usage * 100:.1f}%")
+                f"CPU KV cache usage: {stats.cpu_cache_usage * 100:.1f}%"
+            )
 
             # Reset tracked stats for next interval.
             self.num_prompt_tokens = []
