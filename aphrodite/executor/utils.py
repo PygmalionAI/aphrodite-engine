96cf5de8b9b6345db811c10c7b8e66195f2e936a
AlpinDale <alpindale@gmail.com>
2024-04-02 16:32:59 +0000

    restore the max seqlen log

--- aphrodite/executor/utils.py
+++ aphrodite/executor/utils.py
@@ -1,9 +1,13 @@
+from loguru import logger
+
 def check_block_size_valid(num_gpu_blocks, block_size, max_model_len) -> None:
     if num_gpu_blocks <= 0:
         raise ValueError("No available memory for the cache blocks. "
                          "Try increasing `gpu_memory_utilization` when "
                          "initializing the engine.")
     max_seq_len = block_size * num_gpu_blocks
+    logger.info(f"Maximum sequence length allowed in the cache: "
+                    f"{max_seq_len}")
     if max_model_len > max_seq_len:
         raise ValueError(
             f"The model's max seq len ({max_model_len}) "
