b3323bdac1bb7ec283f9bda07ea90894ed04fff6
AlpinDale <alpindale@gmail.com>
2024-04-02 16:35:46 +0000

    pesky yapf

--- aphrodite/executor/utils.py
+++ aphrodite/executor/utils.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
+
 def check_block_size_valid(num_gpu_blocks, block_size, max_model_len) -> None:
     if num_gpu_blocks <= 0:
         raise ValueError("No available memory for the cache blocks. "
@@ -7,7 +8,7 @@ def check_block_size_valid(num_gpu_blocks, block_size, max_model_len) -> None:
                          "initializing the engine.")
     max_seq_len = block_size * num_gpu_blocks
     logger.info(f"Maximum sequence length allowed in the cache: "
-                    f"{max_seq_len}")
+                f"{max_seq_len}")
     if max_model_len > max_seq_len:
         raise ValueError(
             f"The model's max seq len ({max_model_len}) "
