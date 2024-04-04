860250150b0ee1f1a7699d406d62cb31b81fed5a
AlpinDale <alpindale@gmail.com>
2024-04-04 22:39:19 +0000

    pipe in logitproc in lora

--- aphrodite/lora/layers.py
+++ aphrodite/lora/layers.py
@@ -10,7 +10,6 @@ from transformers import PretrainedConfig
 
 from aphrodite.common.config import LoRAConfig
 from aphrodite.lora.punica import add_lora, add_lora_slice, bgmv
-from aphrodite.modeling.layers.sampler import Sampler
 from aphrodite.modeling.megatron.communication_op import (
     tensor_model_parallel_all_gather,
     tensor_model_parallel_all_reduce,
@@ -20,6 +19,7 @@ from aphrodite.modeling.layers.linear import (ColumnParallelLinear,
                                               RowParallelLinear,
                                               QKVParallelLinear,
                                               MergedColumnParallelLinear)
+from aphrodite.modeling.layers.logits_processor import LogitsProcessor
 from aphrodite.modeling.layers.vocab_parallel_embedding import (
     VocabParallelEmbedding, ParallelLMHead)
 from aphrodite.modeling.megatron.parallel_state import (
@@ -809,11 +809,11 @@ class RowParallelLinearWithLoRA(BaseLayerWithLoRA):
         return self.base_layer.weight
 
 
-class SamplerWithLoRA(BaseLayerWithLoRA):
+class LogitsProcessorWithLoRA(BaseLayerWithLoRA):
 
     def __init__(
         self,
-        base_layer: Sampler,
+        base_layer: LogitsProcessor,
         hidden_size: int,
         dtype: torch.dtype,
         device: torch.device,
@@ -831,6 +831,10 @@ class SamplerWithLoRA(BaseLayerWithLoRA):
     @property
     def vocab_size(self):
         return self.base_layer.vocab_size
+    
+    @property
+    def scale(self):
+        return self.base_layer.scale
 
     @property
     def org_vocab_size(self):
@@ -995,14 +999,14 @@ def from_layer(
     return layer
 
 
-def from_layer_sampler(
-    layer: Sampler,
+def from_layer_logits_processor(
+    layer: LogitsProcessor,
     lm_head: ParallelLMHead,
     max_loras: int,
     lora_config: LoRAConfig,
     model_config: Optional[PretrainedConfig] = None,
-) -> SamplerWithLoRA:
-    ret = SamplerWithLoRA(layer, lm_head.embedding_dim, lm_head.weight.dtype,
-                          lm_head.weight.device)
+) -> LogitsProcessorWithLoRA:
+    ret = LogitsProcessorWithLoRA(layer, lm_head.embedding_dim,
+                                  lm_head.weight.dtype, lm_head.weight.device)
     ret.create_lora_weights(max_loras, lora_config, model_config)
     return ret
--- aphrodite/lora/models.py
+++ aphrodite/lora/models.py
@@ -14,7 +14,7 @@ from aphrodite.common.config import LoRAConfig
 from aphrodite.common.utils import LRUCache, in_wsl
 
 from aphrodite.lora.layers import (BaseLayerWithLoRA, LoRAMapping, from_layer,
-                                   from_layer_sampler)
+                                   from_layer_logits_processor)
 from aphrodite.lora.lora import LoRALayerWeights, PackedLoRALayerWeights
 from aphrodite.lora.utils import parse_fine_tuned_lora_name, replace_submodule
 
@@ -423,11 +423,14 @@ class LoRAModelManager:
                            self.model.config))
             # (yard1): TODO make this more robust
             if "lm_head" in module_name:
-                sampler_module = self.model.get_submodule("sampler")
+                logits_processor_module = self.model.get_submodule(
+                    "logits_processor")
                 new_module = replace_submodule(
-                    self.model, "sampler",
-                    from_layer_sampler(sampler_module, module, self.lora_slots,
-                                       self.lora_config, self.model.config))
+                    self.model, "logits_processor",
+                    from_layer_logits_processor(logits_processor_module,
+                                                module, self.lora_slots,
+                                                self.lora_config,
+                                                self.model.config))
             self.register_module(module_name, new_module)
             self._register_packed_modules(module_name)
             new_module.set_mapping(self.base_indices, self.sampler_indices,
