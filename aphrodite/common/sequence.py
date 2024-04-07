aa55dbe1e016bdf40973f894ffc952c21d726edf
AlpinDale <alpindale@gmail.com>
2024-04-07 01:42:58 +0000

    add logprob ranks

--- aphrodite/common/sequence.py
+++ aphrodite/common/sequence.py
@@ -15,9 +15,14 @@ if TYPE_CHECKING:
 
 @dataclass
 class Logprob:
-    """Infos for supporting OpenAI compatible logprobs."""
-
+    """Infos for supporting OpenAI compatible logprobs and token ranks.
+    Attributes:
+        logprob: The logprob of chosen token
+        rank: The vocab rank of chosen token (>=1)
+        decoded_token: The decoded chosen token index
+    """
     logprob: float
+    rank: Optional[int] = None
     decoded_token: Optional[str] = None
 
 
