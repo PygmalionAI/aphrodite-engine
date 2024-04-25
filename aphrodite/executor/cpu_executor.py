f9f13a348d85568c2b60219ceeef2cfab6a55633
AlpinDale <alpindale@gmail.com>
2024-04-25 01:15:45 +0000

    fix CPU blocks logger for CPU backend

--- aphrodite/executor/cpu_executor.py
+++ aphrodite/executor/cpu_executor.py
@@ -88,7 +88,10 @@ class CPUExecutor(ExecutorBase):
         # greater than one. We could log in the engine, but not all executors
         # have GPUs.
 
-        logger.info(f"# CPU blocks: {num_cpu_blocks}")
+        # NOTE: `cpu block` for CPU backend is located on CPU memory but is
+        # referred as `gpu block`. Because we want to reuse the existing block
+        # management procedure.
+        logger.info(f"# CPU blocks: {num_gpu_blocks}")
         self.driver_worker.initialize_cache(num_gpu_blocks, num_cpu_blocks)
 
     def execute_model(
