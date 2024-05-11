444d26106be5b2b076ccc72000700f4a9da8139b
AlpinDale <alpindale@gmail.com>
2024-05-11 22:19:22 +0000

    add TODO for build script

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -8,6 +8,7 @@ PATH=${cuda_home}/bin:$PATH
 LD_LIBRARY_PATH=${cuda_home}/lib64:$LD_LIBRARY_PATH
 
 # Limit Ninja's number of processes to 1 to prevent OOM
+# TODO: Can we do 2 jobs?
 export MAX_JOBS=1
 
 export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
