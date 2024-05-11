31df81e4ba85effc29abd2f9362d338905eb1f5f
AlpinDale <alpindale@gmail.com>
2024-05-11 19:12:51 +0000

    add sm_60 to build ci

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -10,7 +10,7 @@ LD_LIBRARY_PATH=${cuda_home}/lib64:$LD_LIBRARY_PATH
 # Limit Ninja's number of processes to 1 to prevent OOM
 export MAX_JOBS=1
 
-export TORCH_CUDA_ARCH_LIST="6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
+export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 # Install requirements
 $python_executable -m pip install wheel packaging
 $python_executable -m pip install -r requirements-cuda.txt
