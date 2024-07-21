0f7ef9ef7c2636518fe3ad112adf8f32d2c96325
AlpinDale <alpindale@gmail.com>
2024-07-21 19:52:18 +0000

    fix: import in selector

--- aphrodite/attention/backends/rocm_flash_attn.py
+++ aphrodite/attention/backends/rocm_flash_attn.py
@@ -159,7 +159,7 @@ class ROCmFlashAttentionImpl(AttentionImpl):
             "APHRODITE_USE_TRITON_FLASH_ATTN", "True").lower()
                                       in ("true", "1"))
         if self.use_triton_flash_attn:
-            from aphrodite.attention.ops.triton_flash_attention import \
+            from aphrodite.attention.ops.triton_flash_attn import \
                 triton_attention  # noqa: F401
             self.attn_func = triton_attention
             logger.debug("Using Triton FA in ROCmBackend")
