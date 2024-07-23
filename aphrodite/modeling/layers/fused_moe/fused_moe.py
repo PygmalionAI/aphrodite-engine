b565928d3fd124566c36201b9216274bed011594
AlpinDale <alpindale@gmail.com>
2024-07-23 21:41:35 +0000

    fix: compute_dtype in MoE kernel

--- aphrodite/modeling/layers/fused_moe/fused_moe.py
+++ aphrodite/modeling/layers/fused_moe/fused_moe.py
@@ -431,6 +431,8 @@ def fused_moe(
 
     sorted_token_ids, expert_ids, num_tokens_post_padded = moe_align_block_size(
         topk_ids, config['BLOCK_SIZE_M'], E)
+    compute_type = (tl.bfloat16
+                    if hidden_states.dtype == torch.bfloat16 else tl.float16)
 
     invoke_fused_moe_kernel(hidden_states,
                             w1,
@@ -445,7 +447,7 @@ def fused_moe(
                             False,
                             topk_ids.shape[1],
                             config,
-                            compute_type=tl.float16,
+                            compute_type=compute_dtype,
                             use_fp8=use_fp8)
 
     ops.silu_and_mul(intermediate_cache2, intermediate_cache1.view(-1, N))
@@ -463,7 +465,7 @@ def fused_moe(
                             True,
                             1,
                             config,
-                            compute_type=tl.float16,
+                            compute_type=compute_dtype,
                             use_fp8=use_fp8)
 
     if inplace:
