fd1ce09581be127dfb316a9cc9bcfbfb1cd0015b
AlpinDale <alpindale@gmail.com>
2024-07-24 02:12:53 +0000

    allow multiple tp groups

--- aphrodite/executor/distributed_gpu_executor.py
+++ aphrodite/executor/distributed_gpu_executor.py
@@ -39,8 +39,8 @@ class DistributedGPUExecutor(GPUExecutor):
         # NOTE: We log here to avoid multiple logs when number of workers is
         # greater than one. We could log in the engine, but not all executors
         # have GPUs.
-        logger.info("# GPU blocks: %d, # CPU blocks: %d", num_gpu_blocks,
-                    num_cpu_blocks)
+        logger.info(f"# GPU blocks: {num_gpu_blocks}, "
+                    f"# CPU blocks: {num_cpu_blocks}")
         logger.info(
             f"Minimum concurrency: {num_gpu_blocks * self.cache_config.block_size / self.scheduler_config.max_model_len:.2f}x"  # noqa: E501
         )
