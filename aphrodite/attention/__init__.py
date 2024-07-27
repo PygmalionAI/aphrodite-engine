8670a47a6c11851c92fdb4e2766257a5536fe203
AlpinDale <alpindale@gmail.com>
2024-07-27 02:49:30 +0000

    wip

--- aphrodite/attention/__init__.py
+++ aphrodite/attention/__init__.py
@@ -1,7 +1,6 @@
 from aphrodite.attention.backends.abstract import (
     AttentionBackend,
     AttentionMetadata,
-    AttentionMetadataPerStage,
 )
 from aphrodite.attention.layer import Attention
 from aphrodite.attention.selector import get_attn_backend
@@ -11,5 +10,4 @@ __all__ = [
     "AttentionMetadata",
     "Attention",
     "get_attn_backend",
-    "AttentionMetadataPerStage",
 ]
