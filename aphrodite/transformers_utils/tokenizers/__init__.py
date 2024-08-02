5884e0b90491933bfaa9f746bb239a9e461961f0
AlpinDale <alpindale@gmail.com>
2024-08-02 23:18:44 +0000

    add bitnetforcausallm support

--- aphrodite/transformers_utils/tokenizers/__init__.py
+++ aphrodite/transformers_utils/tokenizers/__init__.py
@@ -1,3 +1,4 @@
 from aphrodite.transformers_utils.tokenizers.baichuan import BaichuanTokenizer
+from aphrodite.transformers_utils.tokenizers.bitnet import BitnetTokenizer
 
-__all__ = ["BaichuanTokenizer"]
+__all__ = ["BaichuanTokenizer", "BitnetTokenizer"]
