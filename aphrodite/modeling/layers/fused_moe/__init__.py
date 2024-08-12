f5b393de6f6cd825101905cafebc18d844bc8c4b
AlpinDale <alpindale@gmail.com>
2024-08-12 20:33:25 +0000

    add deepseek_v2 support

--- aphrodite/modeling/layers/fused_moe/__init__.py
+++ aphrodite/modeling/layers/fused_moe/__init__.py
@@ -1,9 +1,10 @@
 from aphrodite.modeling.layers.fused_moe.fused_moe import (
-    fused_experts, fused_moe, fused_topk, get_config_file_name)
+    fused_experts, fused_moe, fused_topk, get_config_file_name, grouped_topk)
 
 __all__ = [
     "fused_moe",
     "fused_topk",
     "fused_experts",
     "get_config_file_name",
+    "grouped_topk",
 ]
