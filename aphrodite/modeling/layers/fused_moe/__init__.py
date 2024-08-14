cf472315ccd62ec57882a0b9cb59b17872283f9c
AlpinDale <alpindale@gmail.com>
2024-08-14 19:41:57 +0000

    refactor: isolate FP8 from mixtral

--- aphrodite/modeling/layers/fused_moe/__init__.py
+++ aphrodite/modeling/layers/fused_moe/__init__.py
@@ -1,5 +1,7 @@
 from aphrodite.modeling.layers.fused_moe.fused_moe import (
     fused_experts, fused_moe, fused_topk, get_config_file_name, grouped_topk)
+from aphrodite.modeling.layers.fused_moe.layer import (FusedMoE,
+                                                       FusedMoEMethodBase)
 
 __all__ = [
     "fused_moe",
@@ -7,4 +9,6 @@ __all__ = [
     "fused_experts",
     "get_config_file_name",
     "grouped_topk",
+    "FusedMoE",
+    "FusedMoEMethodBase",
 ]
