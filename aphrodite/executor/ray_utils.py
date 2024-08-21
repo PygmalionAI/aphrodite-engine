5240c0da23c3ac7df244377a6613b38c269cdcf5
AlpinDale <alpindale@gmail.com>
2024-08-21 15:58:25 +0000

    fix: avoid unnecessary ray import warnings

--- aphrodite/executor/ray_utils.py
+++ aphrodite/executor/ray_utils.py
@@ -39,9 +39,24 @@ try:
             output = pickle.dumps(output)
             return output
 
+    ray_import_err = None
+
 except ImportError as e:
     ray = None  # type: ignore
+    ray_import_err = e
     RayWorkerWrapper = None  # type: ignore
+
+
+def ray_is_available() -> bool:
+    """Returns True if Ray is available."""
+    return ray is not None
+
+
+def assert_ray_available():
+    """Raise an exception if Ray is not available."""
+    if ray is None:
+        raise ValueError("Failed to import Ray, please install Ray with "
+                         "`pip install ray`.") from ray_import_err
 
 
 def initialize_ray_cluster(
@@ -59,11 +74,7 @@ def initialize_ray_cluster(
         ray_address: The address of the Ray cluster. If None, uses
             the default Ray cluster address.
     """
-    if ray is None:
-        raise ImportError(
-            "Ray is not installed. Please install Ray to use multi-node "
-            "serving. You can install Ray by running "
-            "`pip install aphrodite-engine[\"ray\"]`.")
+    assert_ray_available()
 
     # Connect to a ray cluster.
     if is_hip() or is_xpu():
