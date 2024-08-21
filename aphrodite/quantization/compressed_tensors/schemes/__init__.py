6e561ecda9f5eb8f05fe395a82fa83550c112a27
AlpinDale <alpindale@gmail.com>
2024-08-21 18:15:56 +0000

    chore: clean up `CompressedTensorsW8A8`

--- aphrodite/quantization/compressed_tensors/schemes/__init__.py
+++ aphrodite/quantization/compressed_tensors/schemes/__init__.py
@@ -3,10 +3,7 @@ from .compressed_tensors_unquantized import \
     CompressedTensorsUnquantized  # noqa: F401
 from .compressed_tensors_w4a16_24 import (  # noqa: F401
     W4A16SPARSE24_SUPPORTED_BITS, CompressedTensorsW4A16Sparse24)
-from .compressed_tensors_w8a8_dynamictoken import \
-    CompressedTensorsW8A8DynamicToken  # noqa: F401, E501
-from .compressed_tensors_w8a8_statictensor import \
-    CompressedTensorsW8A8StaticTensor  # noqa: F401, E501
+from .compressed_tensors_w8a8 import CompressedTensorsW8A8  # noqa: F401
 from .compressed_tensors_wNa16 import (
     WNA16_SUPPORTED_BITS,  # noqa: F401
     CompressedTensorsWNA16)  # noqa: F401
