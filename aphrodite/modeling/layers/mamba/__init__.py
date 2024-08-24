2dfa4e47e63afff1a2c91b58a995f8df28567098
AlpinDale <alpindale@gmail.com>
2024-08-24 13:34:10 +0000

    chore: set seed for dummy weights init

--- aphrodite/modeling/layers/mamba/__init__.py
+++ aphrodite/modeling/layers/mamba/__init__.py
@@ -3,7 +3,6 @@ from aphrodite.modeling.layers.mamba.ops.causal_conv1d import (
 from aphrodite.modeling.layers.mamba.ops.mamba_ssm import (
     selective_scan_fn, selective_state_update)
 
-
 __all__ = [
     'causal_conv1d_fn',
     'causal_conv1d_update',
