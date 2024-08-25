c8f5424d72dedb99ab5bdc84181386551e694c99
AlpinDale <alpindale@gmail.com>
2024-08-25 05:00:03 +0000

    add scale_ub inputs to fp8 dynamic per-token quant

--- kernels/quantization/quant_ops.h
+++ kernels/quantization/quant_ops.h
@@ -116,6 +116,6 @@ void static_scaled_fp8_quant(torch::Tensor& out, torch::Tensor const& input,
 void dynamic_scaled_fp8_quant(torch::Tensor& out, torch::Tensor const& input,
                               torch::Tensor& scale);
 
-void dynamic_per_token_scaled_fp8_quant(torch::Tensor& out,
-                                        torch::Tensor const& input,
-                                        torch::Tensor& scale);
+void dynamic_per_token_scaled_fp8_quant(
+    torch::Tensor& out, torch::Tensor const& input, torch::Tensor& scale,
+    c10::optional<torch::Tensor> const& scale_ub);
