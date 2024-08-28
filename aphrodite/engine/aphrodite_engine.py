386ad8bef6e78c28d1a9094fcb8e08d378f4f42a
AlpinDale <alpindale@gmail.com>
2024-08-28 21:09:53 +0000

    feat: tensor parallelism for TPU with ray

--- aphrodite/engine/aphrodite_engine.py
+++ aphrodite/engine/aphrodite_engine.py
@@ -341,8 +341,14 @@ class AphroditeEngine:
             from aphrodite.executor.neuron_executor import NeuronExecutor
             executor_class = NeuronExecutor
         elif engine_config.device_config.device_type == "tpu":
-            from aphrodite.executor.tpu_executor import TPUExecutor
-            executor_class = TPUExecutor
+            if distributed_executor_backend == "ray":
+                initialize_ray_cluster(engine_config.parallel_config)
+                from aphrodite.executor.ray_tpu_executor import RayTPUExecutor
+                executor_class = RayTPUExecutor
+            else:
+                assert distributed_executor_backend is None
+                from aphrodite.executor.tpu_executor import TPUExecutor
+                executor_class = TPUExecutor
         elif engine_config.device_config.device_type == "cpu":
             from aphrodite.executor.cpu_executor import CPUExecutor
             executor_class = CPUExecutor
