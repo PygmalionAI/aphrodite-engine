9cf1275f03a46debde716209e55893bfe84f1644
AlpinDale <alpindale@gmail.com>
2024-08-28 23:36:12 +0000

    feat: add internvl support

--- examples/vision/vision_example.py
+++ examples/vision/vision_example.py
@@ -118,6 +118,22 @@ def run_blip2(question):
     return llm, prompt
 
 
+# InternVL
+def run_internvl(question):
+    # Generally, InternVL can use chatml template for conversation
+    TEMPLATE = "<|im_start|>User\n{prompt}<|im_end|>\n<|im_start|>Assistant\n"
+    prompt = f"<image>\n{question}\n"
+    prompt = TEMPLATE.format(prompt=prompt)
+    llm = LLM(
+        model="OpenGVLab/InternVL2-4B",
+        trust_remote_code=True,
+        max_num_seqs=28,
+        tensor_parallel_size=2,
+        max_model_len=8192,
+    )
+    return llm, prompt
+
+
 model_example_map = {
     "llava": run_llava,
     "llava-next": run_llava_next,
@@ -127,6 +143,7 @@ model_example_map = {
     "chameleon": run_chameleon,
     "minicpmv": run_minicpmv,
     "blip-2": run_blip2,
+    "internvl_chat": run_internvl,
 }
 
 
