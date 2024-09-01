614ca6b0bfff93e75fb2389ae01e7cf236137fbb
AlpinDale <alpindale@gmail.com>
2024-09-01 05:00:17 +0000

    feat: support logits soft capping with flash attention backend

--- aphrodite/attention/backends/abstract.py
+++ aphrodite/attention/backends/abstract.py
@@ -151,6 +151,7 @@ class AttentionImpl(ABC, Generic[T]):
         sliding_window: Optional[int] = None,
         kv_cache_dtype: str = "auto",
         blocksparse_params: Optional[Dict[str, Any]] = None,
+        logits_soft_cap: Optional[float] = None,
     ) -> None:
         raise NotImplementedError
 
