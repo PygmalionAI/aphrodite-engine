bb82d5aee1522c3c586dd3b540ec958a0a9c4638
AlpinDale <alpindale@gmail.com>
2024-09-06 05:04:40 +0000

    ci: exclude cu118 from build and add py_limited_api

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -17,4 +17,4 @@ export MAX_JOBS=2
 export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 
 # Build
-$python_executable setup.py bdist_wheel --dist-dir=dist
+$python_executable setup.py bdist_wheel --dist-dir=dist --py-limited-api=cp38
