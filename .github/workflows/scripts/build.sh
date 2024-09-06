db81a67c543753f1fcd3a17d10b20ac03f77f5ab
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2024-09-06 03:00:15 +0000

    bump to v0.6.0.post1 (#635)

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -7,14 +7,14 @@ cuda_home=/usr/local/cuda-$2
 PATH=${cuda_home}/bin:$PATH
 LD_LIBRARY_PATH=${cuda_home}/lib64:$LD_LIBRARY_PATH
 
-# Limit Ninja's number of processes to 1 to prevent OOM
-# TODO: Can we do 2 jobs?
-export MAX_JOBS=1
-
-export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 # Install requirements
 $python_executable -m pip install wheel packaging
 $python_executable -m pip install -r requirements-cuda.txt
+
+# Limit the number of parallel jobs to avoid OOM
+export MAX_JOBS=2
+
+export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 
 # Build
 $python_executable setup.py bdist_wheel --dist-dir=dist
