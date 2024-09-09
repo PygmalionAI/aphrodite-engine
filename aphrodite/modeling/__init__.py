d67ce116452f7b4b02b2e83c208b5bf74e377e42
AlpinDale <alpindale@gmail.com>
2024-09-09 04:04:24 +0000

    wip

--- aphrodite/modeling/__init__.py
+++ aphrodite/modeling/__init__.py
@@ -1,10 +1,12 @@
 from aphrodite.modeling.parameter import (BaseAphroditeParameter,
                                           PackedAphroditeParameter)
-from aphrodite.modeling.sampling_metadata import SamplingMetadata
+from aphrodite.modeling.sampling_metadata import (SamplingMetadata,
+                                                  SamplingMetadataCache)
 from aphrodite.modeling.utils import set_random_seed
 
 __all__ = [
     "SamplingMetadata",
+    "SamplingMetadataCache",
     "set_random_seed",
     "BaseAphroditeParameter",
     "PackedAphroditeParameter",
