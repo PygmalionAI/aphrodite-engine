b4962740ad23361ccda059e9cf825fa1da55d5c8
AlpinDale <alpindale@gmail.com>
2024-11-01 11:52:27 +0000

    feat: add HQQ quantization support

--- aphrodite/modeling/layers/linear.py
+++ aphrodite/modeling/layers/linear.py
@@ -25,7 +25,7 @@ from aphrodite.quantization.base_config import (QuantizationConfig,
 
 WEIGHT_LOADER_V2_SUPPORTED = [
     "CompressedTensorsLinearMethod", "GPTQMarlinLinearMethod",
-    "AWQMarlinLinearMethod", "AWQLinearMethod",
+    "AWQMarlinLinearMethod", "AWQLinearMethod", "HQQMarlinMethod",
 ]
 
 
