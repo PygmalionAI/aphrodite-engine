140505191220641a7057650a88162de978ef5bc7
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2024-12-04 06:24:44 -0800

    attention: add `AttentionState` abstraction (#863)

--- aphrodite/attention/backends/torch_sdpa.py
+++ aphrodite/attention/backends/torch_sdpa.py
@@ -10,7 +10,8 @@ from aphrodite.attention.backends.abstract import (AttentionBackend,
                                                    AttentionImpl,
                                                    AttentionMetadata,
                                                    AttentionType)
-from aphrodite.attention.backends.utils import CommonMetadataBuilder
+from aphrodite.attention.backends.utils import (CommonAttentionState,
+                                                CommonMetadataBuilder)
 from aphrodite.attention.ops.paged_attn import PagedAttentionMetadata
 from aphrodite.common.utils import is_cpu
 
@@ -36,7 +37,11 @@ class TorchSDPABackend(AttentionBackend):
     @staticmethod
     def get_metadata_cls() -> Type["AttentionMetadata"]:
         return TorchSDPAMetadata
-    
+
+    @staticmethod
+    def get_state_cls() -> Type["CommonAttentionState"]:
+        return CommonAttentionState
+
     @staticmethod
     def get_builder_cls() -> Type["TorchSDPAMetadataBuilder"]:
         return TorchSDPAMetadataBuilder
