4f9fea4c4dd3eef73d6c1778c564308905f4ff9e
Naomiusearch <121130001+Naomiusearch@users.noreply.github.com>
2024-12-05 02:40:01 +0100

    fix: ROCm build (#817)
    * Some fixed (ig)
    
    * Oopsie :3
    
    * Remove a comment
    
    * indent the block
    
    * another indentation
    
    * Revert stuff (hopefully)
    
    ---------
    
    Co-authored-by: AlpinDale <52078762+AlpinDale@users.noreply.github.com>
    Co-authored-by: AlpinDale <alpindale@gmail.com>

--- amdpatch.sh
+++ amdpatch.sh
@@ -2,4 +2,4 @@
 
 ROCM_PATH=$(hipconfig --rocmpath)
 
-sudo patch $ROCM_PATH/lib/llvm/lib/clang/18/include/__clang_hip_cmath.h ./patches/amd.patch
+sudo patch $ROCM_PATH/lib/llvm/lib/clang/*/include/__clang_hip_cmath.h ./patches/amd.patch
