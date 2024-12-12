b6fd3c8ec4228194a31e46b5922d15d82d3be988
AlpinDale <alpindale@gmail.com>
2024-12-12 04:33:35 +0000

    api: better startup failure UX

--- tests/endpoints/openai/test_mp_api_server.py
+++ tests/endpoints/openai/test_mp_api_server.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from aphrodite.common.utils import FlexibleArgumentParser
@@ -8,19 +10,19 @@ from aphrodite.endpoints.openai.args import make_arg_parser
 @pytest.mark.asyncio
 async def test_mp_crash_detection():
 
-    with pytest.raises(RuntimeError) as excinfo:
-        parser = FlexibleArgumentParser(
-            description="Aphrodite's remote OpenAI server.")
-        parser = make_arg_parser(parser)
-        args = parser.parse_args([])
-        # use an invalid tensor_parallel_size to trigger the
-        # error in the server
-        args.tensor_parallel_size = 65536
-
-        async with build_async_engine_client(args):
-            pass
-    assert "The server process died before responding to the readiness probe"\
-          in str(excinfo.value)
+    parser = FlexibleArgumentParser(
+        description="Aphrodite's remote OpenAI server.")
+    parser = make_arg_parser(parser)
+    args = parser.parse_args([])
+    # use an invalid tensor_parallel_size to trigger the
+    # error in the server
+    args.tensor_parallel_size = 65536
+    start = time.perf_counter()
+    async with build_async_engine_client(args):
+        pass
+    end = time.perf_counter()
+    assert end - start < 60, ("Expected Aphrodite to gracefully shutdown in "
+                              "<60s if there is an error in the startup.")
 
 
 @pytest.mark.asyncio
