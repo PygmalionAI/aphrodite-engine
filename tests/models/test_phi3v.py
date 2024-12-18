aa31d528690aec3ed6a70e701e3fbe864003c872
AlpinDale <alpindale@gmail.com>
2024-12-18 16:52:16 +0000

    feat: multi-image input support for Phi3V

--- tests/models/test_phi3v.py
+++ tests/models/test_phi3v.py
@@ -21,13 +21,15 @@ HF_IMAGE_PROMPTS = IMAGE_ASSETS.prompts({
     "cherry_blossom":
     "<|user|>\n<|image_1|>\nWhat is the season?<|end|>\n<|assistant|>\n",
 })
+HF_MULTIIMAGE_IMAGE_PROMPT = "<|user|>\n<|image_1|>\n<|image_2|>\nDescribe these images.<|end|>\n<|assistant|>\n"  # noqa: E501
 
 models = ["microsoft/Phi-3.5-vision-instruct"]
 
 
-def aphrodite_to_hf_output(aphrodite_output: Tuple[List[int], str,
-                                         Optional[SampleLogprobs]],
-                      model: str):
+def aphrodite_to_hf_output(
+    aphrodite_output: Tuple[List[int], str, Optional[SampleLogprobs]],
+    model: str,
+):
     """Sanitize aphrodite output to be comparable with hf output."""
     _, output_str, out_logprobs = aphrodite_output
 
@@ -73,19 +75,22 @@ def run_test(
 
     All the image fixtures for the test is under tests/images.
     For huggingface runner, we provide the PIL images as input.
-    For aphrodite runner, we provide MultiModalDataDict objects 
+    For aphrodite runner, we provide MultiModalDataDict objects
     and corresponding MultiModalConfig as input.
     Note, the text input is also adjusted to abide by aphrodite contract.
     The text output is sanitized to be able to compare with hf.
     """
 
-    inputs_per_image = [(
-        [prompt for _ in size_factors],
-        [
-            rescale_image_size(image, factor, transpose=idx)
-            for idx, factor in enumerate(size_factors)
-        ],
-    ) for image, prompt in zip(images, HF_IMAGE_PROMPTS)]
+    inputs_per_image = [
+        (
+            [prompt for _ in size_factors],
+            [
+                rescale_image_size(image, factor, transpose=idx)
+                for idx, factor in enumerate(size_factors)
+            ],
+        )
+        for image, prompt in zip(images, HF_IMAGE_PROMPTS)
+    ]
 
     # NOTE: take care of the order. run Aphrodite first, and then run HF.
     # Aphrodite needs a fresh new process without cuda initialization.
@@ -93,37 +98,42 @@ def run_test(
     # will hurt multiprocessing backend with fork method (the default method).
 
     # max_model_len should be greater than image_feature_size
-    with aphrodite_runner(model,
-                     max_model_len=4096,
-                     max_num_seqs=1,
-                     dtype=dtype,
-                     tensor_parallel_size=tensor_parallel_size,
-                     distributed_executor_backend=distributed_executor_backend,
-                     enforce_eager=True) as aphrodite_model:
+    with aphrodite_runner(
+        model,
+        max_model_len=4096,
+        max_num_seqs=1,
+        dtype=dtype,
+        tensor_parallel_size=tensor_parallel_size,
+        distributed_executor_backend=distributed_executor_backend,
+        enforce_eager=True,
+    ) as aphrodite_model:
         aphrodite_outputs_per_image = [
-            aphrodite_model.generate_greedy_logprobs(prompts,
-                                                max_tokens,
-                                                num_logprobs=num_logprobs,
-                                                images=images)
+            aphrodite_model.generate_greedy_logprobs(
+                prompts, max_tokens, num_logprobs=num_logprobs, images=images
+            )
             for prompts, images in inputs_per_image
         ]
 
     # use eager mode for hf runner, since phi3_v didn't work with flash_attn
     hf_model_kwargs = {"_attn_implementation": "eager"}
-    with hf_runner(model, dtype=dtype,
-                   model_kwargs=hf_model_kwargs) as hf_model:
+    with hf_runner(
+        model, dtype=dtype, model_kwargs=hf_model_kwargs
+    ) as hf_model:
         eos_token_id = hf_model.processor.eos_token_id
         hf_outputs_per_image = [
-            hf_model.generate_greedy_logprobs_limit(prompts,
-                                                    max_tokens,
-                                                    num_logprobs=num_logprobs,
-                                                    images=images,
-                                                    eos_token_id=eos_token_id)
+            hf_model.generate_greedy_logprobs_limit(
+                prompts,
+                max_tokens,
+                num_logprobs=num_logprobs,
+                images=images,
+                eos_token_id=eos_token_id,
+            )
             for prompts, images in inputs_per_image
         ]
 
-    for hf_outputs, aphrodite_outputs in zip(hf_outputs_per_image,
-                                        aphrodite_outputs_per_image):
+    for hf_outputs, aphrodite_outputs in zip(
+        hf_outputs_per_image, aphrodite_outputs_per_image
+    ):
         check_logprobs_close(
             outputs_0_lst=hf_outputs,
             outputs_1_lst=[
@@ -154,8 +164,16 @@ def run_test(
 @pytest.mark.parametrize("dtype", [target_dtype])
 @pytest.mark.parametrize("max_tokens", [128])
 @pytest.mark.parametrize("num_logprobs", [10])
-def test_models(hf_runner, aphrodite_runner, image_assets, model, size_factors,
-                dtype: str, max_tokens: int, num_logprobs: int) -> None:
+def test_models(
+    hf_runner,
+    aphrodite_runner,
+    image_assets,
+    model,
+    size_factors,
+    dtype: str,
+    max_tokens: int,
+    num_logprobs: int,
+) -> None:
     run_test(
         hf_runner,
         aphrodite_runner,
@@ -171,8 +189,9 @@ def test_models(hf_runner, aphrodite_runner, image_assets, model, size_factors,
 
 @pytest.mark.parametrize("model", models)
 @pytest.mark.parametrize("dtype", [target_dtype])
-def test_regression_7840(hf_runner, aphrodite_runner, image_assets, model,
-                         dtype) -> None:
+def test_regression_7840(
+    hf_runner, aphrodite_runner, image_assets, model, dtype
+) -> None:
     run_test(
         hf_runner,
         aphrodite_runner,
@@ -182,5 +201,125 @@ def test_regression_7840(hf_runner, aphrodite_runner, image_assets, model,
         dtype=dtype,
         max_tokens=128,
         num_logprobs=10,
+        tensor_parallel_size=1,
+    )
+
+
+def run_multi_image_test(
+    hf_runner: Type[HfRunner],
+    aphrodite_runner: Type[AphroditeRunner],
+    images: List[Image.Image],
+    model: str,
+    *,
+    size_factors: List[float],
+    dtype: str,
+    max_tokens: int,
+    num_logprobs: int,
+    tensor_parallel_size: int,
+    distributed_executor_backend: Optional[str] = None,
+):
+    """Inference result should be the same between hf and aphrodite.
+    All the image fixtures for the test is under tests/images.
+    For huggingface runner, we provide the PIL images as input.
+    For aphrodite runner, we provide MultiModalDataDict objects
+    and corresponding MultiModalConfig as input.
+    Note, the text input is also adjusted to abide by aphrodite contract.
+    The text output is sanitized to be able to compare with hf.
+    """
+    inputs_per_case = [
+        (
+            [HF_MULTIIMAGE_IMAGE_PROMPT for _ in size_factors],
+            [
+                [rescale_image_size(image, factor) for image in images]
+                for factor in size_factors
+            ],
+        )
+    ]
+    # NOTE: take care of the order. run Aphrodite first, and then run HF.
+    # Aphrodite needs a fresh new process without cuda initialization.
+    # if we run HF first, the cuda initialization will be done and it
+    # will hurt multiprocessing backend with fork method (the default method).
+    # max_model_len should be greater than image_feature_size
+    with aphrodite_runner(
+        model,
+        max_model_len=4096,
+        max_num_seqs=1,
+        limit_mm_per_prompt={"image": len(images)},
+        dtype=dtype,
+        tensor_parallel_size=tensor_parallel_size,
+        distributed_executor_backend=distributed_executor_backend,
+        enforce_eager=True,
+    ) as aphrodite_model:
+        aphrodite_outputs_per_case = [
+            aphrodite_model.generate_greedy_logprobs(
+                prompts, max_tokens, num_logprobs=num_logprobs, images=images
+            )
+            for prompts, images in inputs_per_case
+        ]
+    hf_model_kwargs = {"_attn_implementation": "eager"}
+    with hf_runner(
+        model, dtype=dtype, model_kwargs=hf_model_kwargs
+    ) as hf_model:
+        eos_token_id = hf_model.processor.eos_token_id
+        hf_outputs_per_case = [
+            hf_model.generate_greedy_logprobs_limit(
+                prompts,
+                max_tokens,
+                num_logprobs=num_logprobs,
+                images=images,
+                eos_token_id=eos_token_id,
+            )
+            for prompts, images in inputs_per_case
+        ]
+    for hf_outputs, aphrodite_outputs in zip(
+        hf_outputs_per_case, aphrodite_outputs_per_case
+    ):
+        check_logprobs_close(
+            outputs_0_lst=hf_outputs,
+            outputs_1_lst=[
+                aphrodite_to_hf_output(aphrodite_output, model)
+                for aphrodite_output in aphrodite_outputs
+            ],
+            name_0="hf",
+            name_1="aphrodite",
+        )
+
+
+@pytest.mark.parametrize("model", models)
+@pytest.mark.parametrize(
+    "size_factors",
+    [
+        # No image
+        [],
+        # Single-scale
+        [1.0],
+        # Single-scale, batched
+        [1.0, 1.0, 1.0],
+        # Multi-scale
+        [0.25, 0.5, 1.0],
+    ],
+)
+@pytest.mark.parametrize("dtype", [target_dtype])
+@pytest.mark.parametrize("max_tokens", [128])
+@pytest.mark.parametrize("num_logprobs", [5])
+def test_multi_images_models(
+    hf_runner,
+    aphrodite_runner,
+    image_assets,
+    model,
+    size_factors,
+    dtype: str,
+    max_tokens: int,
+    num_logprobs: int,
+) -> None:
+    run_multi_image_test(
+        hf_runner,
+        aphrodite_runner,
+        [asset.pil_image for asset in image_assets],
+        model,
+        size_factors=size_factors,
+        dtype=dtype,
+        max_tokens=max_tokens,
+        num_logprobs=num_logprobs,
         tensor_parallel_size=1,
     )
