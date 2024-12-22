34e8606e81ac48b39adbc03fb53e76be55e8c342
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2024-12-21 18:05:44 -0800

    vlm: do not allow max_model_len overflow (#962)

--- tests/models/test_llava.py
+++ tests/models/test_llava.py
@@ -179,3 +179,18 @@ def test_models(hf_runner, aphrodite_runner, image_assets, model, size_factors,
         num_logprobs=num_logprobs,
         tensor_parallel_size=1,
     )
+
+
+@pytest.mark.parametrize("model", models)
+def test_context_length_too_short(aphrodite_runner, image_assets, model):
+    images = [asset.pil_image for asset in image_assets]
+    with pytest.raises(ValueError, match="too long to fit into the model"):
+        aphrodite_model = aphrodite_runner(
+            model,
+            max_model_len=128,  # LLaVA has a feature size of 576
+            enforce_eager=True,
+        )
+        with aphrodite_model:
+            aphrodite_model.generate_greedy([HF_IMAGE_PROMPTS[0]],
+                                       max_tokens=1,
+                                       images=[images[0]])
