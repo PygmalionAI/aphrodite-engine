809e0d86b8180560cbb8f3359260df92d3f00c3b
AlpinDale <alpindale@gmail.com>
2024-12-26 08:04:07 +0000

    cpu: add support for W8A8 quantization via compressed-tensor

---
+++ aphrodite/platforms/cpu.py
@@ -0,0 +1,13 @@
+import torch
+
+from .interface import Platform, PlatformEnum
+
+
+class CpuPlatform(Platform):
+    _enum = PlatformEnum.CPU
+    @staticmethod
+    def get_device_name(device_id: int = 0) -> str:
+        return "cpu"
+    @staticmethod
+    def inference_mode():
+        return torch.no_grad()
