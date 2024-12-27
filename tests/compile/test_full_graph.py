f768a2f22e94514634a4cd9e23c4a28c82de9e4a
AlpinDale <alpindale@gmail.com>
2024-12-27 06:01:38 +0000

    torch.compile: register all-reduce operations as custom ops

--- tests/compile/test_full_graph.py
+++ tests/compile/test_full_graph.py
@@ -2,9 +2,18 @@ import os
 
 import pytest
 
+from aphrodite.common.utils import cuda_device_count_stateless
+
+from ..utils import fork_new_process_for_each_test
+
 
 @pytest.mark.parametrize("model", ["meta-llama/Meta-Llama-3-8B"])
-def test_full_graph(model):
+@pytest.mark.parametrize("tp_size", [1, 2])
+@fork_new_process_for_each_test
+def test_full_graph(model, tp_size):
+    # Skip the test if there are not enough CUDA devices.
+    if cuda_device_count_stateless() < tp_size:
+        pytest.skip("Not enough CUDA devices for the test.")
     # make sure these models can be captured in full graph mode
     os.environ["APHRODITE_TEST_DYNAMO_GRAPH_CAPTURE"] = "1"
 
@@ -16,7 +25,7 @@ def test_full_graph(model):
         "The future of AI is",
     ]
     sampling_params = SamplingParams(temperature=0)
-    llm = LLM(model=model, enforce_eager=True)
+    llm = LLM(model=model, enforce_eager=True, tensor_parallel_size=tp_size)
     outputs = llm.generate(prompts, sampling_params)
     # Print the outputs.
     for output in outputs:
